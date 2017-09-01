406d8736e81aa6251dd4315ab3770c796ca3f0a6
Donald Adu-Poku <donald.adu@gmail.com>
2017-09-01 23:53:48 +0000

    rpcserver: add batched request support (json 2.0)

--- dcrjson/dcrwalletextcmds_test.go
+++ dcrjson/dcrwalletextcmds_test.go
@@ -34,7 +34,7 @@ func TestDcrWalletExtCmds(t *testing.T) {
 	for i, test := range tests {
 		// Marshal the command as created by the new static command
 		// creation function.
-		marshalled, err := dcrjson.MarshalCmd(testID, test.staticCmd())
+		marshalled, err := dcrjson.MarshalCmd("1.0", testID, test.staticCmd())
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)
@@ -58,7 +58,7 @@ func TestDcrWalletExtCmds(t *testing.T) {
 
 		// Marshal the command as created by the generic new command
 		// creation function.
-		marshalled, err = dcrjson.MarshalCmd(testID, cmd)
+		marshalled, err = dcrjson.MarshalCmd("1.0", testID, cmd)
 		if err != nil {
 			t.Errorf("MarshalCmd #%d (%s) unexpected error: %v", i,
 				test.name, err)
