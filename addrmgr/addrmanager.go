d76bb9452c098e9147d12d5ef754df254f96f026
Sarlor <kinsleer@outlook.com>
2018-08-16 11:16:23 +0800

    Update addrmanager.go
    addrmgr/addrmanager.go: This corrects the comment is Dcrd instead of Bitcoind.

--- addrmgr/addrmanager.go
+++ addrmgr/addrmanager.go
@@ -247,7 +247,7 @@ func (a *AddrManager) updateAddress(netAddr, srcAddr *wire.NetAddress) {
 func (a *AddrManager) expireNew(bucket int) {
 	// First see if there are any entries that are so bad we can just throw
 	// them away. otherwise we throw away the oldest entry in the cache.
-	// Bitcoind here chooses four random and just throws the oldest of
+	// Dcrd here chooses four random and just throws the oldest of
 	// those away, but we keep track of oldest in the initial traversal and
 	// use that information instead.
 	var oldest *KnownAddress
@@ -285,7 +285,7 @@ func (a *AddrManager) expireNew(bucket int) {
 }
 
 // pickTried selects an address from the tried bucket to be evicted.
-// We just choose the eldest. Bitcoind selects 4 random entries and throws away
+// We just choose the eldest. Dcrd selects 4 random entries and throws away
 // the older of them.
 func (a *AddrManager) pickTried(bucket int) *list.Element {
 	var oldest *KnownAddress
