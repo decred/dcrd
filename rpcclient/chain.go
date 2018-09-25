2b7942847028b39e31242089ccf898e9b16fb7fe
Jonathan Chappelow <chappjc@protonmail.com>
2018-09-25 12:49:49 -0500

    rpcclient: support getchaintips
    This adds support for the getchaintips RPC to rpcclient, adding
    asynchronous and blocking functions to rpcclient.Client.

--- rpcclient/chain.go
+++ rpcclient/chain.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2014-2016 The btcsuite developers
-// Copyright (c) 2015-2017 The Decred developers
+// Copyright (c) 2015-2018 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
@@ -546,6 +546,42 @@ func (r FutureVerifyChainResult) Receive() (bool, error) {
 		return false, err
 	}
 	return verified, nil
+}
+
+// FutureGetChainTipsResult is a future promise to deliver the result of a
+// GetChainTipsAsync RPC invocation (or an applicable error).
+type FutureGetChainTipsResult chan *response
+
+// Receive waits for the response promised by the future and returns slice of
+// all known tips in the block tree.
+func (r FutureGetChainTipsResult) Receive() ([]dcrjson.GetChainTipsResult, error) {
+	res, err := receiveFuture(r)
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal the result.
+	var chainTips []dcrjson.GetChainTipsResult
+	err = json.Unmarshal(res, &chainTips)
+	if err != nil {
+		return nil, err
+	}
+	return chainTips, nil
+}
+
+// GetChainTipsAsync returns an instance of a type that can be used to get the
+// result of the RPC at some future time by invoking the Receive function on the
+// returned instance.
+//
+// See GetChainTips for the blocking version and more details.
+func (c *Client) GetChainTipsAsync() FutureGetChainTipsResult {
+	cmd := dcrjson.NewGetChainTipsCmd()
+	return c.sendCmd(cmd)
+}
+
+// GetChainTips returns all known tips in the block tree.
+func (c *Client) GetChainTips() ([]dcrjson.GetChainTipsResult, error) {
+	return c.GetChainTipsAsync().Receive()
 }
 
 // VerifyChainAsync returns an instance of a type that can be used to get the
