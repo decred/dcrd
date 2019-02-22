bbada7521aaf5ea31fac0368f57b06ce1dd4f2e0
Hamid <7atashfeshan@gmail.com>
2019-02-23 01:13:04 +0330

    wire: Fix maximum payload length of MsgAddr.
    This fixes the calculation of the maximum payload size of MsgAddr.
    The number of address in encoded MsgAddr is VarInt which its size for
    the maximum size of allowed address is lower than MaxVarIntPayload.

--- wire/msgaddr.go
+++ wire/msgaddr.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2013-2015 The btcsuite developers
-// Copyright (c) 2015-2016 The Decred developers
+// Copyright (c) 2015-2019 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
@@ -120,8 +120,11 @@ func (msg *MsgAddr) Command() string {
 // MaxPayloadLength returns the maximum length the payload can be for the
 // receiver.  This is part of the Message interface implementation.
 func (msg *MsgAddr) MaxPayloadLength(pver uint32) uint32 {
-	// Num addresses (varInt) + max allowed addresses.
-	return MaxVarIntPayload + (MaxAddrPerMsg * maxNetAddressPayload(pver))
+	// Num addresses (size of varInt for max address per message) + max allowed
+	// addresses * max address size.
+	return uint32(VarIntSerializeSize(MaxAddrPerMsg)) +
+		(MaxAddrPerMsg * maxNetAddressPayload(pver))
+
 }
 
 // NewMsgAddr returns a new bitcoin addr message that conforms to the
