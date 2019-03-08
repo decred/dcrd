f65a8a0626aba16e4a6d312cfe1705f9ed97f534
Hamid <7atashfeshan@gmail.com>
2019-03-08 12:03:58 +0330

    dcrec/secp256k1: Update README.md broken link.

--- dcrec/secp256k1/doc.go
+++ dcrec/secp256k1/doc.go
@@ -1,15 +1,14 @@
 // Copyright (c) 2013-2014 The btcsuite developers
-// Copyright (c) 2015-2016 The Decred developers
+// Copyright (c) 2015-2019 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
 /*
-Package secp256k1 implements support for the elliptic curves needed for decred.
+Package secp256k1 implements support for the elliptic curves needed for Decred.
 
 Decred uses elliptic curve cryptography using koblitz curves
 (specifically secp256k1) for cryptographic functions.  See
-http://www.secg.org/collateral/sec2_final.pdf for details on the
-standard.
+http://www.secg.org/sec2-v2.pdf for details on the standard.
 
 This package provides the data structures and functions implementing the
 crypto/elliptic Curve interface in order to permit using these curves
