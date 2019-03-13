c596826688798b7e1d0955279dc9e9c0cafdb4c8
Dave Collins <davec@conformal.com>
2019-03-13 01:12:13 -0500

    txscript: Optimize CalcMultiSigStats.
    This converts the CalcMultiSigStats function to make use of the new
    extractMultisigScriptDetails function instead of the far less efficient
    parseScript thereby significantly optimizing the function.
    
    The tests are also updated accordingly.
    
    The following is a before and after comparison of analyzing a standard
    multisig script:
    
    benchmark                    old ns/op    new ns/op    delta
    ---------------------------------------------------------------
    BenchmarkCalcMultiSigStats   972          79.5         -91.82%
    
    benchmark                    old allocs   new allocs   delta
    ---------------------------------------------------------------
    BenchmarkCalcMultiSigStats   1            0            -100.00%
    
    benchmark                    old bytes    new bytes    delta
    ---------------------------------------------------------------
    BenchmarkCalcMultiSigStats   2304         0            -100.00%

--- txscript/standard_test.go
+++ txscript/standard_test.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2013-2017 The btcsuite developers
-// Copyright (c) 2015-2018 The Decred developers
+// Copyright (c) 2015-2019 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
@@ -739,7 +739,7 @@ func TestCalcMultiSigStats(t *testing.T) {
 			name: "short script",
 			script: "0x046708afdb0fe5548271967f1a67130b7105cd6a828" +
 				"e03909a67962e0ea1f61d",
-			err: scriptError(ErrMalformedPush, ""),
+			err: scriptError(ErrNotMultisigScript, ""),
 		},
 		{
 			name: "stack underflow",
@@ -750,11 +750,7 @@ func TestCalcMultiSigStats(t *testing.T) {
 		},
 		{
 			name: "multisig script",
-			script: "0 DATA_72 0x30450220106a3e4ef0b51b764a2887226" +
-				"2ffef55846514dacbdcbbdd652c849d395b4384022100" +
-				"e03ae554c3cbb40600d31dd46fc33f25e47bf8525b1fe" +
-				"07282e3b6ecb5f3bb2801 CODESEPARATOR 1 DATA_33 " +
-				"0x0232abdc893e7f0631364d7fd01cb33d24da45329a0" +
+			script: "1 DATA_33 0x0232abdc893e7f0631364d7fd01cb33d24da45329a0" +
 				"0357b3a7886211ab414d55a 1 CHECKMULTISIG",
 			err: nil,
 		},
