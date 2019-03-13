123a733665d4f8b556377616a47790ff5030274a
Dave Collins <davec@conformal.com>
2019-03-13 01:12:02 -0500

    txscript: Add bench for stake change scripts.

--- txscript/bench_test.go
+++ txscript/bench_test.go
@@ -407,3 +407,18 @@ func BenchmarkIsStakeRevocationScript(b *testing.B) {
 		_ = isStakeRevocationScript(scriptVersion, script)
 	}
 }
+
+// BenchmarkIsStakeChangeScript benchmarks how long it takes to analyze a very
+// large script to determine if it is a standard stake change script.
+func BenchmarkIsStakeChangeScript(b *testing.B) {
+	script, err := genComplexScript()
+	if err != nil {
+		b.Fatalf("failed to create benchmark script: %v", err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		pops, _ := parseScript(script)
+		_ = isSStxChange(pops)
+	}
+}
