e6a5701daeb377f564e97dc09bee9a64724b6cc5
Dave Collins <davec@conformal.com>
2019-03-13 01:11:00 -0500

    txscript: Move init func in benchmarks to top.

--- txscript/bench_test.go
+++ txscript/bench_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 The Decred developers
+// Copyright (c) 2018-2019 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
@@ -22,20 +22,6 @@ var (
 	prevOutScript = hexToBytes("a914f5916158e3e2c4551c1796708db8367207ed13bb87")
 )
 
-// BenchmarkCalcSigHash benchmarks how long it takes to calculate the signature
-// hashes for all inputs of a transaction with many inputs.
-func BenchmarkCalcSigHash(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		for j := 0; j < len(manyInputsBenchTx.TxIn); j++ {
-			_, err := CalcSignatureHash(prevOutScript, SigHashAll,
-				&manyInputsBenchTx, j, nil)
-			if err != nil {
-				b.Fatalf("failed to calc signature hash: %v", err)
-			}
-		}
-	}
-}
-
 func init() {
 	// tx 620f57c92cf05a7f7e7f7d28255d5f7089437bc48e34dcfebf7751d08b7fb8f5
 	txHex, err := ioutil.ReadFile("data/many_inputs_tx.hex")
@@ -47,5 +33,19 @@ func init() {
 	err = manyInputsBenchTx.Deserialize(bytes.NewReader(txBytes))
 	if err != nil {
 		panic(err)
+	}
+}
+
+// BenchmarkCalcSigHash benchmarks how long it takes to calculate the signature
+// hashes for all inputs of a transaction with many inputs.
+func BenchmarkCalcSigHash(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < len(manyInputsBenchTx.TxIn); j++ {
+			_, err := CalcSignatureHash(prevOutScript, SigHashAll,
+				&manyInputsBenchTx, j, nil)
+			if err != nil {
+				b.Fatalf("failed to calc signature hash: %v", err)
+			}
+		}
 	}
 }
