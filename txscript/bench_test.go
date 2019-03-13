95175cb873e899a5a7c86d8734a3952bd3c0f166
Dave Collins <davec@conformal.com>
2019-03-13 01:11:47 -0500

    txscript: Add bench for pay-to-alt-pubkey-hash scripts.

--- txscript/bench_test.go
+++ txscript/bench_test.go
@@ -333,3 +333,18 @@ func BenchmarkIsPubKeyHashScript(b *testing.B) {
 		_ = isPubKeyHashScript(script)
 	}
 }
+
+// BenchmarkIsAltPubKeyHashScript benchmarks how long it takes to analyze a very
+// large script to determine if it is a standard pay-to-alt-pubkey-hash script.
+func BenchmarkIsAltPubKeyHashScript(b *testing.B) {
+	script, err := genComplexScript()
+	if err != nil {
+		b.Fatalf("failed to create benchmark script: %v", err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		pops, _ := parseScript(script)
+		_ = isPubkeyHashAlt(pops)
+	}
+}
