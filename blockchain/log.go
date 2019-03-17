be1d82ea8266fe557cb5cc65969ce4ba715f1d53
David Hill <dhill@mindcry.org>
2019-03-17 13:05:40 -0400

    multi: drop init and just set default log

--- blockchain/log.go
+++ blockchain/log.go
@@ -12,12 +12,8 @@ import (
 // log is a logger that is initialized with no output filters.  This
 // means the package will not perform any logging by default until the caller
 // requests it.
-var log slog.Logger
-
 // The default amount of logging is none.
-func init() {
-	DisableLog()
-}
+var log = slog.Disabled
 
 // DisableLog disables all library log output.  Logging output is disabled
 // by default until UseLogger is called.
--- fees/log.go
+++ fees/log.go
@@ -10,12 +10,8 @@ import (
 
 // log is a logger that is initialized with no output filters.  This means the
 // package will not perform any logging by default until the caller requests it.
-var log slog.Logger
-
 // The default amount of logging is none.
-func init() {
-	DisableLog()
-}
+var log = slog.Disabled
 
 // DisableLog disables all library log output.  Logging output is disabled by
 // default until either UseLogger or SetLogWriter are called.
