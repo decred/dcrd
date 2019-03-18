3d2d557614e51a58f88b1e610ba340a5885811cd
David Hill <dhill@mindcry.org>
2019-03-18 11:30:31 -0400

    multi: deprecate DisableLog

--- fees/log.go
+++ fees/log.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 The Decred developers
+// Copyright (c) 2018-2019 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
@@ -13,8 +13,10 @@ import (
 // The default amount of logging is none.
 var log = slog.Disabled
 
-// DisableLog disables all library log output.  Logging output is disabled by
-// default until either UseLogger or SetLogWriter are called.
+// DisableLog disables all library log output.  Logging output is disabled
+// by default until UseLogger is called.
+//
+// Deprecated: Use UseLogger(slog.Disabled) instead.
 func DisableLog() {
 	log = slog.Disabled
 }
--- mempool/log.go
+++ mempool/log.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2013-2016 The btcsuite developers
-// Copyright (c) 2016-2018 The Decred developers
+// Copyright (c) 2016-2019 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
@@ -16,7 +16,9 @@ import (
 var log = slog.Disabled
 
 // DisableLog disables all library log output.  Logging output is disabled
-// by default until either UseLogger or SetLogWriter are called.
+// by default until UseLogger is called.
+//
+// Deprecated: Use UseLogger(slog.Disabled) instead.
 func DisableLog() {
 	log = slog.Disabled
 }
