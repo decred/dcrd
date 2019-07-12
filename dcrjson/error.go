5a6461087cac87625bacc26d2a25279e75549024
Josh Rickmar <jrick@companyzero.com>
2019-07-12 16:30:41 -0400

    Rewrite dcrjson/v2 in terms of dcrjson/v3 and types
    All compatible types have been aliased to the appropriate types from
    the dcrjson/v3 and types module. The following types were not aliased
    due to breaking changes:
    
    ```
    $ go doc github.com/decred/dcrd/dcrjson/v2 | grep '^type [A-Z]' | grep -v =
    type ExistsExpiredTicketsCmd struct{ ... }
    type ExistsLiveTicketsCmd struct{ ... }
    type ExistsMempoolTxsCmd struct{ ... }
    type ExistsMissedTicketsCmd struct{ ... }
    type GetHeadersCmd struct{ ... }
    type RescanCmd struct{ ... }
    ```

--- dcrjson/error.go
+++ dcrjson/error.go
@@ -5,13 +5,11 @@
 
 package dcrjson
 
-import (
-	"fmt"
-)
+import v3 "github.com/decred/dcrd/dcrjson/v3"
 
 // ErrorCode identifies a kind of error.  These error codes are NOT used for
 // JSON-RPC response errors.
-type ErrorCode int
+type ErrorCode = v3.ErrorCode
 
 // These constants are used to identify a specific RuleError.
 const (
@@ -67,46 +65,9 @@ const (
 	numErrorCodes
 )
 
-// Map of ErrorCode values back to their constant names for pretty printing.
-var errorCodeStrings = map[ErrorCode]string{
-	ErrDuplicateMethod:      "ErrDuplicateMethod",
-	ErrInvalidUsageFlags:    "ErrInvalidUsageFlags",
-	ErrInvalidType:          "ErrInvalidType",
-	ErrEmbeddedType:         "ErrEmbeddedType",
-	ErrUnexportedField:      "ErrUnexportedField",
-	ErrUnsupportedFieldType: "ErrUnsupportedFieldType",
-	ErrNonOptionalField:     "ErrNonOptionalField",
-	ErrNonOptionalDefault:   "ErrNonOptionalDefault",
-	ErrMismatchedDefault:    "ErrMismatchedDefault",
-	ErrUnregisteredMethod:   "ErrUnregisteredMethod",
-	ErrMissingDescription:   "ErrMissingDescription",
-	ErrNumParams:            "ErrNumParams",
-}
-
-// String returns the ErrorCode as a human-readable name.
-func (e ErrorCode) String() string {
-	if s := errorCodeStrings[e]; s != "" {
-		return s
-	}
-	return fmt.Sprintf("Unknown ErrorCode (%d)", int(e))
-}
-
 // Error identifies a general error.  This differs from an RPCError in that this
 // error typically is used more by the consumers of the package as opposed to
 // RPCErrors which are intended to be returned to the client across the wire via
 // a JSON-RPC Response.  The caller can use type assertions to determine the
 // specific error and access the ErrorCode field.
-type Error struct {
-	Code    ErrorCode // Describes the kind of error
-	Message string    // Human readable description of the issue
-}
-
-// Error satisfies the error interface and prints human-readable errors.
-func (e Error) Error() string {
-	return e.Message
-}
-
-// makeError creates an Error given a set of arguments.
-func makeError(c ErrorCode, desc string) Error {
-	return Error{Code: c, Message: desc}
-}
+type Error = v3.Error
--- dcrjson/jsonrpc.go
+++ dcrjson/jsonrpc.go
@@ -5,40 +5,22 @@
 
 package dcrjson
 
-import (
-	"encoding/json"
-	"fmt"
-)
+import v3 "github.com/decred/dcrd/dcrjson/v3"
 
 // RPCErrorCode represents an error code to be used as a part of an RPCError
 // which is in turn used in a JSON-RPC Response object.
 //
 // A specific type is used to help ensure the wrong errors aren't used.
-type RPCErrorCode int
+type RPCErrorCode = v3.RPCErrorCode
 
 // RPCError represents an error that is used as a part of a JSON-RPC Response
 // object.
-type RPCError struct {
-	Code    RPCErrorCode `json:"code,omitempty"`
-	Message string       `json:"message,omitempty"`
-}
-
-// Guarantee RPCError satisifies the builtin error interface.
-var _, _ error = RPCError{}, (*RPCError)(nil)
-
-// Error returns a string describing the RPC error.  This satisifies the
-// builtin error interface.
-func (e RPCError) Error() string {
-	return fmt.Sprintf("%d: %s", e.Code, e.Message)
-}
+type RPCError = v3.RPCError
 
 // NewRPCError constructs and returns a new JSON-RPC error that is suitable
 // for use in a JSON-RPC Response object.
 func NewRPCError(code RPCErrorCode, message string) *RPCError {
-	return &RPCError{
-		Code:    code,
-		Message: message,
-	}
+	return v3.NewRPCError(code, message)
 }
 
 // IsValidIDType checks that the ID field (which can go in any of the JSON-RPC
@@ -49,16 +31,7 @@ func NewRPCError(code RPCErrorCode, message string) *RPCError {
 // marshalling for some reason.    The functions which accept an ID in this
 // package already call this function to ensure the provided id is valid.
 func IsValidIDType(id interface{}) bool {
-	switch id.(type) {
-	case int, int8, int16, int32, int64,
-		uint, uint8, uint16, uint32, uint64,
-		float32, float64,
-		string,
-		nil:
-		return true
-	default:
-		return false
-	}
+	return v3.IsValidIDType(id)
 }
 
 // Request represents raw JSON-RPC requests.  The Method field identifies the
@@ -67,57 +40,7 @@ func IsValidIDType(id interface{}) bool {
 // typed command infrastructure which handles creation of these requests,
 // however this struct is being exported in case the caller wants to construct
 // raw requests for some reason.
-type Request struct {
-	Jsonrpc string            `json:"jsonrpc"`
-	Method  string            `json:"method"`
-	Params  []json.RawMessage `json:"params"`
-	ID      interface{}       `json:"id"`
-}
-
-// UnmarshalJSON is a custom unmarshal func for the Request struct. The param
-// field defaults to an empty json.RawMessage array it is omitted by the request
-// or nil if the supplied value is invalid.
-func (request *Request) UnmarshalJSON(b []byte) error {
-	var data map[string]interface{}
-	err := json.Unmarshal(b, &data)
-	if err != nil {
-		return err
-	}
-
-	request.ID = data["id"]
-	methodValue, hasMethod := data["method"]
-	if hasMethod {
-		request.Method = methodValue.(string)
-	}
-	jsonrpcValue, hasJsonrpc := data["jsonrpc"]
-	if hasJsonrpc {
-		request.Jsonrpc = jsonrpcValue.(string)
-	}
-	paramsValue, hasParams := data["params"]
-	if !hasParams {
-		// set the request param to an empty array if it is omitted in the request
-		request.Params = []json.RawMessage{}
-	}
-	if hasParams {
-		// assert the request params is an array of data
-		params, paramsOk := paramsValue.([]interface{})
-		if paramsOk {
-			rawParams := make([]json.RawMessage, 0, len(params))
-			for _, param := range params {
-				marshalledParam, err := json.Marshal(param)
-				if err != nil {
-					return err
-				}
-				rawMessage := json.RawMessage(marshalledParam)
-				rawParams = append(rawParams, rawMessage)
-			}
-
-			request.Params = rawParams
-		}
-	}
-
-	return nil
-}
+type Request = v3.Request
 
 // NewRequest returns a new JSON-RPC request object given the provided rpc
 // version, id, method, and parameters.  The parameters are marshalled into a
@@ -128,44 +51,14 @@ func (request *Request) UnmarshalJSON(b []byte) error {
 // MarshalCmd function with that command to generate the marshalled JSON-RPC
 // request.
 func NewRequest(rpcVersion string, id interface{}, method string, params []interface{}) (*Request, error) {
-	// default to JSON-RPC 1.0 if RPC type is not specified
-	if rpcVersion != "2.0" && rpcVersion != "1.0" {
-		rpcVersion = "1.0"
-	}
-
-	if !IsValidIDType(id) {
-		str := fmt.Sprintf("the id of type '%T' is invalid", id)
-		return nil, makeError(ErrInvalidType, str)
-	}
-
-	rawParams := make([]json.RawMessage, 0, len(params))
-	for _, param := range params {
-		marshalledParam, err := json.Marshal(param)
-		if err != nil {
-			return nil, err
-		}
-		rawMessage := json.RawMessage(marshalledParam)
-		rawParams = append(rawParams, rawMessage)
-	}
-
-	return &Request{
-		Jsonrpc: rpcVersion,
-		ID:      id,
-		Method:  method,
-		Params:  rawParams,
-	}, nil
+	return v3.NewRequest(rpcVersion, id, method, params)
 }
 
 // Response is the general form of a JSON-RPC response.  The type of the
 // Result field varies from one command to the next, so it is implemented as an
 // interface.  The ID field has to be a pointer to allow for a nil value when
 // empty.
-type Response struct {
-	Jsonrpc string          `json:"jsonrpc"`
-	Result  json.RawMessage `json:"result"`
-	Error   *RPCError       `json:"error"`
-	ID      *interface{}    `json:"id"`
-}
+type Response = v3.Response
 
 // NewResponse returns a new JSON-RPC response object given the provided rpc
 // version, id, marshalled result, and RPC error.  This function is only
@@ -173,39 +66,12 @@ type Response struct {
 // Typically callers will instead want to create the fully marshalled JSON-RPC
 // response to send over the wire with the MarshalResponse function.
 func NewResponse(rpcVersion string, id interface{}, marshalledResult []byte, rpcErr *RPCError) (*Response, error) {
-	if rpcVersion != "2.0" && rpcVersion != "1.0" {
-		rpcVersion = "1.0"
-	}
-
-	if !IsValidIDType(id) {
-		str := fmt.Sprintf("the id of type '%T' is invalid", id)
-		return nil, makeError(ErrInvalidType, str)
-	}
-
-	pid := &id
-	return &Response{
-		Jsonrpc: rpcVersion,
-		Result:  marshalledResult,
-		Error:   rpcErr,
-		ID:      pid,
-	}, nil
+	return v3.NewResponse(rpcVersion, id, marshalledResult, rpcErr)
 }
 
 // MarshalResponse marshals the passed rpc version, id, result, and RPCError to
 // a JSON-RPC response byte slice that is suitable for transmission to a
 // JSON-RPC client.
 func MarshalResponse(rpcVersion string, id interface{}, result interface{}, rpcErr *RPCError) ([]byte, error) {
-	if rpcVersion != "2.0" && rpcVersion != "1.0" {
-		rpcVersion = "1.0"
-	}
-
-	marshalledResult, err := json.Marshal(result)
-	if err != nil {
-		return nil, err
-	}
-	response, err := NewResponse(rpcVersion, id, marshalledResult, rpcErr)
-	if err != nil {
-		return nil, err
-	}
-	return json.Marshal(&response)
+	return v3.MarshalResponse(rpcVersion, id, result, rpcErr)
 }
