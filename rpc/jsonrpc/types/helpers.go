82d92e575796ea5d4f9a01c8e9794a2d8610c63d
Dave Collins <davec@conformal.com>
2019-07-21 03:40:26 -0500

    jsonrpc/types: Update copyright years.
    This updates the copyright years that were missed during the conversion
    from dcrjson to the types module.

--- rpc/jsonrpc/types/helpers.go
+++ rpc/jsonrpc/types/helpers.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2019 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
 package types
 
 // EstimateSmartFeeModeAddr is a helper routine that allocates a new
