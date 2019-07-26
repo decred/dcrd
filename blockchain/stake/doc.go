813ed1902d45e89c679df4bdb598208099e07e4c
Dave Collins <davec@conformal.com>
2019-07-26 13:18:04 -0500

    stake: Make doc.go formatting consistent.

--- blockchain/stake/doc.go
+++ blockchain/stake/doc.go
@@ -1,16 +1,15 @@
 // Copyright (c) 2013-2014 The btcsuite developers
-// Copyright (c) 2015-2016 The Decred developers
+// Copyright (c) 2015-2019 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
 /*
-Package stake contains code for all of dcrd's stake
-transaction chain handling and other portions related to the
-Proof-of-Stake (PoS) system.
+Package stake contains code for all of dcrd's stake transaction chain handling
+and other portions related to the Proof-of-Stake (PoS) system.
 
-At the heart of the PoS system are tickets, votes and revocations.
-These 3 pieces work together to determine if previous blocks are
-valid and their txs should be confirmed.
+At the heart of the PoS system are tickets, votes and revocations.  These 3
+pieces work together to determine if previous blocks are valid and their txs
+should be confirmed.
 
 Important Parts included in stake package:
 
@@ -18,7 +17,5 @@ Important Parts included in stake package:
 - TicketDB
 - Stake Reward calculation
 - Stake transaction identification (IsSStx, IsSSGen, IsSSRtx)
-
-
 */
 package stake
