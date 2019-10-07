1751922a451b443f2e34e6ef02182ef4fe6e2208
Dave Collins <davec@conformal.com>
2019-10-07 15:10:59 -0500

    secp256k1: Redefine in terms of v2 module.
    This redefines the v1 secp256k1 module in terms of the v2 module by
    making use of type aliases.  This will allow the v2 module types to be
    used in place of the v1 types in other modules without requiring them to
    undergo a major version bump.
    
    Thus it updates the dcrec/secp256k1 v1 module dependencies and serves as
    a base for dcrec/secp256k1/v1.0.3.
    
    The updated direct dependencies in this commit are as follows:
    
    - github.com/decred/dcrd/chaincfg/chainhash@v1.0.2
    - github.com/decred/dcrd/dcrec/secp256k1/v2@v2.0.0
    
    The full list of updated direct dependencies since the previous
    dcrec/secp256k1/v1.0.2 release are as follows:
    
    - github.com/decred/dcrd/chaincfg/chainhash@v1.0.2
    - github.com/decred/dcrd/dcrec/secp256k1/v2@v2.0.0

--- dcrec/secp256k1/go.mod
+++ dcrec/secp256k1/go.mod
@@ -1,6 +1,9 @@
 module github.com/decred/dcrd/dcrec/secp256k1
 
+go 1.11
+
 require (
 	github.com/davecgh/go-spew v1.1.1
-	github.com/decred/dcrd/chaincfg/chainhash v1.0.1
+	github.com/decred/dcrd/chaincfg/chainhash v1.0.2
+	github.com/decred/dcrd/dcrec/secp256k1/v2 v2.0.0
 )
--- dcrec/secp256k1/privkey.go
+++ dcrec/secp256k1/privkey.go
@@ -6,117 +6,45 @@
 package secp256k1
 
 import (
-	"crypto/ecdsa"
-	"crypto/rand"
 	"io"
 	"math/big"
+
+	v2 "github.com/decred/dcrd/dcrec/secp256k1/v2"
 )
 
 // PrivateKey wraps an ecdsa.PrivateKey as a convenience mainly for signing
 // things with the the private key without having to directly import the ecdsa
 // package.
-type PrivateKey ecdsa.PrivateKey
+type PrivateKey = v2.PrivateKey
 
 // NewPrivateKey instantiates a new private key from a scalar encoded as a
 // big integer.
 func NewPrivateKey(d *big.Int) *PrivateKey {
-	b := make([]byte, 0, PrivKeyBytesLen)
-	dB := paddedAppend(PrivKeyBytesLen, b, d.Bytes())
-	priv, _ := PrivKeyFromBytes(dB)
-	return priv
+	return v2.NewPrivateKey(d)
 }
 
 // PrivKeyFromBytes returns a private and public key for `curve' based on the
 // private key passed as an argument as a byte slice.
-func PrivKeyFromBytes(pk []byte) (*PrivateKey,
-	*PublicKey) {
-	curve := S256()
-	x, y := curve.ScalarBaseMult(pk)
-
-	priv := &ecdsa.PrivateKey{
-		PublicKey: ecdsa.PublicKey{
-			Curve: S256(),
-			X:     x,
-			Y:     y,
-		},
-		D: new(big.Int).SetBytes(pk),
-	}
-
-	return (*PrivateKey)(priv), (*PublicKey)(&priv.PublicKey)
+func PrivKeyFromBytes(pk []byte) (*PrivateKey, *PublicKey) {
+	return v2.PrivKeyFromBytes(pk)
 }
 
 // PrivKeyFromScalar is the same as PrivKeyFromBytes in secp256k1.
-func PrivKeyFromScalar(s []byte) (*PrivateKey,
-	*PublicKey) {
+func PrivKeyFromScalar(s []byte) (*PrivateKey, *PublicKey) {
 	return PrivKeyFromBytes(s)
 }
 
 // GeneratePrivateKey is a wrapper for ecdsa.GenerateKey that returns a PrivateKey
 // instead of the normal ecdsa.PrivateKey.
 func GeneratePrivateKey() (*PrivateKey, error) {
-	key, err := ecdsa.GenerateKey(S256(), rand.Reader)
-	if err != nil {
-		return nil, err
-	}
-	return (*PrivateKey)(key), nil
+	return v2.GeneratePrivateKey()
 }
 
 // GenerateKey generates a key using a random number generator, returning
 // the private scalar and the corresponding public key points.
-func GenerateKey(rand io.Reader) (priv []byte, x,
-	y *big.Int, err error) {
-	key, err := ecdsa.GenerateKey(S256(), rand)
-	priv = key.D.Bytes()
-	x = key.PublicKey.X
-	y = key.PublicKey.Y
-
-	return
-}
-
-// Public returns the PublicKey corresponding to this private key.
-func (p PrivateKey) Public() (*big.Int, *big.Int) {
-	return p.PublicKey.X, p.PublicKey.Y
-}
-
-// PubKey returns the PublicKey corresponding to this private key.
-func (p *PrivateKey) PubKey() *PublicKey {
-	return (*PublicKey)(&p.PublicKey)
-}
-
-// ToECDSA returns the private key as a *ecdsa.PrivateKey.
-func (p *PrivateKey) ToECDSA() *ecdsa.PrivateKey {
-	return (*ecdsa.PrivateKey)(p)
-}
-
-// Sign generates an ECDSA signature for the provided hash (which should be the
-// result of hashing a larger message) using the private key. Produced signature
-// is deterministic (same message and same key yield the same signature) and
-// canonical in accordance with RFC6979 and BIP0062.
-func (p *PrivateKey) Sign(hash []byte) (*Signature, error) {
-	return signRFC6979(p, hash)
+func GenerateKey(rand io.Reader) ([]byte, *big.Int, *big.Int, error) {
+	return v2.GenerateKey(rand)
 }
 
 // PrivKeyBytesLen defines the length in bytes of a serialized private key.
 const PrivKeyBytesLen = 32
-
-// Serialize returns the private key number d as a big-endian binary-encoded
-// number, padded to a length of 32 bytes.
-func (p PrivateKey) Serialize() []byte {
-	b := make([]byte, 0, PrivKeyBytesLen)
-	return paddedAppend(PrivKeyBytesLen, b, p.ToECDSA().D.Bytes())
-}
-
-// SerializeSecret satisfies the chainec PrivateKey interface.
-func (p PrivateKey) SerializeSecret() []byte {
-	return p.Serialize()
-}
-
-// GetD satisfies the chainec PrivateKey interface.
-func (p PrivateKey) GetD() *big.Int {
-	return p.D
-}
-
-// GetType satisfies the chainec PrivateKey interface.
-func (p PrivateKey) GetType() int {
-	return ecTypeSecp256k1
-}
