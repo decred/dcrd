acb23162032192a11aabeb7ef2a2968634a7671c
David Hill <dhill@mindcry.org>
2019-10-25 14:13:01 -0400

    dcrec: bump values of signature types
    This ensures users explicitly set the signature type.

--- dcrec/common.go
+++ dcrec/common.go
@@ -11,13 +11,13 @@ type SignatureType int
 const (
 	// STEcdsaSecp256k1 specifies that the signature is an ECDSA signature
 	// over the secp256k1 elliptic curve.
-	STEcdsaSecp256k1 SignatureType = 0
+	STEcdsaSecp256k1 SignatureType = 1
 
 	// STEd25519 specifies that the signature is an ECDSA signature over the
 	// edwards25519 twisted Edwards curve.
-	STEd25519 = 1
+	STEd25519 = 2
 
 	// STSchnorrSecp256k1 specifies that the signature is a Schnorr
 	// signature over the secp256k1 elliptic curve.
-	STSchnorrSecp256k1 = 2
+	STSchnorrSecp256k1 = 3
 )
