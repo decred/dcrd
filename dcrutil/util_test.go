8e82835f7862c4fd9c619d44a75c7ffcd58fc808
Dave Collins <davec@conformal.com>
2020-09-25 02:05:10 -0500

    dcrutil: Cleanup verify tests and use mock params.
    This cleans up the tests for verify message and also modifies them to
    use the available mock parameters to avoid an otherwise unnecessary dep
    on chaincfg.

--- dcrutil/util_test.go
+++ dcrutil/util_test.go
@@ -6,36 +6,51 @@ package dcrutil
 
 import (
 	"testing"
-
-	"github.com/decred/dcrd/chaincfg/v3"
 )
 
+// TestVerifyMessage ensures the verifying a message works as intended.
 func TestVerifyMessage(t *testing.T) {
+	mainNetParams := mockMainNetParams()
+	testNetParams := mockTestNetParams()
+
 	msg := "verifymessage test"
 
 	var tests = []struct {
+		name    string
 		addr    string
 		sig     string
 		params  AddressParams
 		isValid bool
-	}{
-		// valid
-		{"TsdbYVDoh3JsyP6oEg2aHVoTjsFuHzUgGKv", "IITPXfmkfLPULbX9Im3XIyHAKiXRw5N9j6P7qf0MdEP9YQinn51lWjS+8jbTceRxCWckKKssu3ZpQm1xCWKz9GA=", chaincfg.TestNet3Params(), true},
+	}{{
+		name:    "valid",
+		addr:    "TsdbYVDoh3JsyP6oEg2aHVoTjsFuHzUgGKv",
+		sig:     "IITPXfmkfLPULbX9Im3XIyHAKiXRw5N9j6P7qf0MdEP9YQinn51lWjS+8jbTceRxCWckKKssu3ZpQm1xCWKz9GA=",
+		params:  testNetParams,
+		isValid: true,
+	}, {
+		name:    "wrong address",
+		addr:    "TsWeG3TJzucZgYyMfZFC2GhBvbeNfA48LTo",
+		sig:     "IITPXfmkfLPULbX9Im3XIyHAKiXRw5N9j6P7qf0MdEP9YQinn51lWjS+8jbTceRxCWckKKssu3ZpQm1xCWKz9GA=",
+		params:  testNetParams,
+		isValid: false,
+	}, {
+		name:    "wrong signature",
+		addr:    "TsdbYVDoh3JsyP6oEg2aHVoTjsFuHzUgGKv",
+		sig:     "HxzZggzHMljSWpKHnw1Dow84KGWvTRBCG2JqBM5W4Q7iePW0dirZXCggSeXHVQ26D0MbDFffi3yw+x2Z5nQ94gg=",
+		params:  testNetParams,
+		isValid: false,
+	}, {
+		name:    "wrong params",
+		addr:    "TsdbYVDoh3JsyP6oEg2aHVoTjsFuHzUgGKv",
+		sig:     "IITPXfmkfLPULbX9Im3XIyHAKiXRw5N9j6P7qf0MdEP9YQinn51lWjS+8jbTceRxCWckKKssu3ZpQm1xCWKz9GA=",
+		params:  mainNetParams,
+		isValid: false,
+	}}
 
-		// wrong address
-		{"TsWeG3TJzucZgYyMfZFC2GhBvbeNfA48LTo", "IITPXfmkfLPULbX9Im3XIyHAKiXRw5N9j6P7qf0MdEP9YQinn51lWjS+8jbTceRxCWckKKssu3ZpQm1xCWKz9GA=", chaincfg.TestNet3Params(), false},
-
-		// wrong signature
-		{"TsdbYVDoh3JsyP6oEg2aHVoTjsFuHzUgGKv", "HxzZggzHMljSWpKHnw1Dow84KGWvTRBCG2JqBM5W4Q7iePW0dirZXCggSeXHVQ26D0MbDFffi3yw+x2Z5nQ94gg=", chaincfg.TestNet3Params(), false},
-
-		// wrong params
-		{"TsdbYVDoh3JsyP6oEg2aHVoTjsFuHzUgGKv", "IITPXfmkfLPULbX9Im3XIyHAKiXRw5N9j6P7qf0MdEP9YQinn51lWjS+8jbTceRxCWckKKssu3ZpQm1xCWKz9GA=", chaincfg.MainNetParams(), false},
-	}
-
-	for i, test := range tests {
+	for _, test := range tests {
 		err := VerifyMessage(test.addr, test.sig, msg, test.params)
 		if (test.isValid && err != nil) || (!test.isValid && err == nil) {
-			t.Fatalf("VerifyMessage test #%d failed", i+1)
+			t.Fatalf("%s: failed", test.name)
 		}
 	}
 }
