86953c4c0d2e524e5fb3091a331add1fb6cd6305
Donald Adu-Poku <donald.adu@gmail.com>
2021-06-13 19:13:31 +0000

    indexers: async exists address index.
    This reworks the ExistsAddressIndex to implement the updated Indexer
    interface for asynchoronous index updates. This index subscribes with
    no prerequisites.

--- blockchain/indexers/existsaddrindex.go
+++ blockchain/indexers/existsaddrindex.go
@@ -6,9 +6,11 @@ package indexers
 
 import (
 	"context"
+	"fmt"
 	"sync"
 
 	"github.com/decred/dcrd/blockchain/stake/v4"
+	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/chaincfg/v3"
 	"github.com/decred/dcrd/database/v3"
 	"github.com/decred/dcrd/dcrutil/v4"
@@ -46,8 +48,9 @@ type ExistsAddrIndex struct {
 	// The following fields are set when the instance is created and can't
 	// be changed afterwards, so there is no need to protect them with a
 	// separate mutex.
-	db          database.DB
-	chainParams *chaincfg.Params
+	db    database.DB
+	chain ChainQueryer
+	sub   *IndexSubscription
 
 	// The following fields are used to quickly link transactions and
 	// addresses that have not been included into a block yet when an
@@ -64,31 +67,72 @@ type ExistsAddrIndex struct {
 	// once they are included into a block.
 	unconfirmedLock sync.RWMutex
 	mpExistsAddr    map[[addrKeySize]byte]struct{}
+
+	subscribers map[chan bool]struct{}
+	mtx         sync.Mutex
+	cancel      context.CancelFunc
 }
 
 // NewExistsAddrIndex returns a new instance of an indexer that is used to
 // create a mapping of all addresses ever seen.
-//
-// It implements the Indexer interface which plugs into the IndexManager that in
-// turn is used by the blockchain package.  This allows the index to be
-// seamlessly maintained along with the chain.
-func NewExistsAddrIndex(db database.DB, chainParams *chaincfg.Params) *ExistsAddrIndex {
-	return &ExistsAddrIndex{
+func NewExistsAddrIndex(subscriber *IndexSubscriber, db database.DB, chain ChainQueryer) (*ExistsAddrIndex, error) {
+	idx := &ExistsAddrIndex{
 		db:           db,
-		chainParams:  chainParams,
+		chain:        chain,
 		mpExistsAddr: make(map[[addrKeySize]byte]struct{}),
+		subscribers:  make(map[chan bool]struct{}),
+		cancel:       subscriber.cancel,
 	}
+
+	// The exists address index is an optional index. It has no
+	// prerequisite and is updated asynchronously.
+	sub, err := subscriber.Subscribe(idx, noPrereqs)
+	if err != nil {
+		return nil, err
+	}
+
+	idx.sub = sub
+
+	err = idx.Init(subscriber.ctx, chain.ChainParams())
+	if err != nil {
+		return nil, err
+	}
+
+	return idx, nil
 }
 
 // Ensure the ExistsAddrIndex type implements the Indexer interface.
 var _ Indexer = (*ExistsAddrIndex)(nil)
 
-// Init is only provided to satisfy the Indexer interface as there is nothing to
-// initialize for this index.
+// Init initializes the exists address transaction index.
 //
 // This is part of the Indexer interface.
-func (idx *ExistsAddrIndex) Init() error {
-	// Nothing to do.
+func (idx *ExistsAddrIndex) Init(ctx context.Context, chainParams *chaincfg.Params) error {
+	if interruptRequested(ctx) {
+		return errInterruptRequested
+	}
+
+	// Finish any drops that were previously interrupted.
+	if err := finishDrop(ctx, idx); err != nil {
+		return err
+	}
+
+	// Create the initial state for the index as needed.
+	if err := createIndex(idx, &chainParams.GenesisHash); err != nil {
+		return err
+	}
+
+	// Upgrade the index as needed.
+	if err := upgradeIndex(ctx, idx, &chainParams.GenesisHash); err != nil {
+		return err
+	}
+
+	// Recover the exists address index and its dependents to the main
+	// chain if needed.
+	if err := recover(ctx, idx); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -113,14 +157,63 @@ func (idx *ExistsAddrIndex) Version() uint32 {
 	return existsAddrIndexVersion
 }
 
-// Create is invoked when the indexer manager determines the index needs
-// to be created for the first time.  It creates the bucket for the address
-// index.
+// DB returns the database of the index.
+//
+// This is part of the Indexer interface.
+func (idx *ExistsAddrIndex) DB() database.DB {
+	return idx.db
+}
+
+// Queryer returns the chain queryer.
+//
+// This is part of the Indexer interface.
+func (idx *ExistsAddrIndex) Queryer() ChainQueryer {
+	return idx.chain
+}
+
+// Tip returns the current tip of the index.
+//
+// This is part of the Indexer interface.
+func (idx *ExistsAddrIndex) Tip() (int64, *chainhash.Hash, error) {
+	return tip(idx.db, idx.Key())
+}
+
+// Create is invoked when the index is created for the first time.
+// It creates the bucket for the address index.
 //
 // This is part of the Indexer interface.
 func (idx *ExistsAddrIndex) Create(dbTx database.Tx) error {
 	_, err := dbTx.Metadata().CreateBucket(existsAddrIndexKey)
 	return err
+}
+
+// IndexSubscription returns the subscription for index updates.
+//
+// This is part of the Indexer interface.
+func (idx *ExistsAddrIndex) IndexSubscription() *IndexSubscription {
+	return idx.sub
+}
+
+// Subscribers returns all client channels waiting for the next index update.
+//
+// This is part of the Indexer interface.
+func (idx *ExistsAddrIndex) Subscribers() map[chan bool]struct{} {
+	idx.mtx.Lock()
+	defer idx.mtx.Unlock()
+	return idx.subscribers
+}
+
+// WaitForSync subscribes clients for the next index sync update.
+//
+// This is part of the Indexer interface.
+func (idx *ExistsAddrIndex) WaitForSync() chan bool {
+	c := make(chan bool)
+
+	idx.mtx.Lock()
+	idx.subscribers[c] = struct{}{}
+	idx.mtx.Unlock()
+
+	return c
 }
 
 // dbPutExistsAddr uses an existing database transaction to update or add a
@@ -211,12 +304,11 @@ func (idx *ExistsAddrIndex) ExistsAddresses(addrs []stdaddr.Address) ([]bool, er
 	return exists, nil
 }
 
-// ConnectBlock is invoked by the index manager when a new block has been
-// connected to the main chain.  This indexer adds a key for each address
-// the transactions in the block involve.
+// connectBlock adds all addresses associated with transactions in the
+// provided block.
 //
 // This is part of the Indexer interface.
-func (idx *ExistsAddrIndex) ConnectBlock(dbTx database.Tx, block, parent *dcrutil.Block, _ PrevScripter, isTreasuryEnabled bool) error {
+func (idx *ExistsAddrIndex) connectBlock(dbTx database.Tx, block, parent *dcrutil.Block, _ PrevScripter, isTreasuryEnabled bool) error {
 	// NOTE: The fact that the block can disapprove the regular tree of the
 	// previous block is ignored for this index because even though technically
 	// the address might become unused again if its only use was in a
@@ -245,7 +337,8 @@ func (idx *ExistsAddrIndex) ConnectBlock(dbTx database.Tx, block, parent *dcruti
 				rs := txscript.MultisigRedeemScriptFromScriptSig(
 					txIn.SignatureScript)
 				class, addrs, _, err := txscript.ExtractPkScriptAddrs(
-					scriptVersion, rs, idx.chainParams, isTreasuryEnabled)
+					scriptVersion, rs, idx.chain.ChainParams(),
+					isTreasuryEnabled)
 				if err != nil {
 					// Non-standard outputs are skipped.
 					continue
@@ -267,8 +360,9 @@ func (idx *ExistsAddrIndex) ConnectBlock(dbTx database.Tx, block, parent *dcruti
 		}
 
 		for _, txOut := range tx.MsgTx().TxOut {
-			class, addrs, _, err := txscript.ExtractPkScriptAddrs(txOut.Version,
-				txOut.PkScript, idx.chainParams, isTreasuryEnabled)
+			class, addrs, _, err := txscript.ExtractPkScriptAddrs(
+				txOut.Version, txOut.PkScript, idx.chain.ChainParams(),
+				isTreasuryEnabled)
 			if err != nil {
 				// Non-standard outputs are skipped.
 				continue
@@ -276,7 +370,7 @@ func (idx *ExistsAddrIndex) ConnectBlock(dbTx database.Tx, block, parent *dcruti
 
 			if isSStx && class == txscript.NullDataTy {
 				addr, err := stake.AddrFromSStxPkScrCommitment(txOut.PkScript,
-					idx.chainParams)
+					idx.chain.ChainParams())
 				if err != nil {
 					// Ignore unsupported address types.
 					continue
@@ -325,15 +419,15 @@ func (idx *ExistsAddrIndex) ConnectBlock(dbTx database.Tx, block, parent *dcruti
 		}
 	}
 
-	return nil
+	// Update the current index tip.
+	return dbPutIndexerTip(dbTx, idx.Key(), block.Hash(), int32(block.Height()))
 }
 
-// DisconnectBlock is invoked by the index manager when a block has been
-// disconnected from the main chain. Note that the exists address manager
-// never removes addresses.
+// disconnectBlock only updates the index tip because the index never removes
+// addresses, even in the case of a reorg.
 //
 // This is part of the Indexer interface.
-func (idx *ExistsAddrIndex) DisconnectBlock(dbTx database.Tx, block, parent *dcrutil.Block, _ PrevScripter, isTreasuryEnabled bool) error {
+func (idx *ExistsAddrIndex) disconnectBlock(dbTx database.Tx, block, parent *dcrutil.Block, _ PrevScripter, _ bool) error {
 	// The primary purpose of this index is to track whether or not addresses
 	// have ever been seen, so even if they ultimately end up technically
 	// becoming unused due to being in a block that was disconnected and the
@@ -346,7 +440,10 @@ func (idx *ExistsAddrIndex) DisconnectBlock(dbTx database.Tx, block, parent *dcr
 	// case and the huge performance gained from not having to constantly update
 	// the index with usage counts that would be required to properly handle
 	// disconnecting block and disapproved regular trees.
-	return nil
+
+	// Update the current index tip.
+	return dbPutIndexerTip(dbTx, idx.Key(), &block.MsgBlock().Header.PrevBlock,
+		int32(block.Height()-1))
 }
 
 // addUnconfirmedTx adds all addresses related to the transaction to the
@@ -363,8 +460,9 @@ func (idx *ExistsAddrIndex) addUnconfirmedTx(tx *wire.MsgTx, isTreasuryEnabled b
 			const scriptVersion = 0
 			rs := txscript.MultisigRedeemScriptFromScriptSig(
 				txIn.SignatureScript)
-			class, addrs, _, err := txscript.ExtractPkScriptAddrs(scriptVersion,
-				rs, idx.chainParams, isTreasuryEnabled)
+			class, addrs, _, err := txscript.ExtractPkScriptAddrs(
+				scriptVersion, rs, idx.chain.ChainParams(),
+				isTreasuryEnabled)
 			if err != nil {
 				// Non-standard outputs are skipped.
 				continue
@@ -389,7 +487,7 @@ func (idx *ExistsAddrIndex) addUnconfirmedTx(tx *wire.MsgTx, isTreasuryEnabled b
 
 	for _, txOut := range tx.TxOut {
 		class, addrs, _, err := txscript.ExtractPkScriptAddrs(txOut.Version,
-			txOut.PkScript, idx.chainParams, isTreasuryEnabled)
+			txOut.PkScript, idx.chain.ChainParams(), isTreasuryEnabled)
 		if err != nil {
 			// Non-standard outputs are skipped.
 			continue
@@ -397,7 +495,7 @@ func (idx *ExistsAddrIndex) addUnconfirmedTx(tx *wire.MsgTx, isTreasuryEnabled b
 
 		if isSStx && class == txscript.NullDataTy {
 			addr, err := stake.AddrFromSStxPkScrCommitment(txOut.PkScript,
-				idx.chainParams)
+				idx.chain.ChainParams())
 			if err != nil {
 				// Ignore unsupported address types.
 				continue
@@ -440,4 +538,32 @@ func DropExistsAddrIndex(ctx context.Context, db database.DB) error {
 // exists.
 func (*ExistsAddrIndex) DropIndex(ctx context.Context, db database.DB) error {
 	return DropExistsAddrIndex(ctx, db)
+}
+
+// ProcessNotification indexes the provided notification based on its
+// notification type.
+//
+// This is part of the Indexer interface.
+func (idx *ExistsAddrIndex) ProcessNotification(dbTx database.Tx, ntfn *IndexNtfn) error {
+	switch ntfn.NtfnType {
+	case ConnectNtfn:
+		err := idx.connectBlock(dbTx, ntfn.Block, ntfn.Parent,
+			ntfn.PrevScripts, ntfn.IsTreasuryEnabled)
+		if err != nil {
+			return fmt.Errorf("%s: unable to connect block: %v", idx.Name(), err)
+		}
+
+	case DisconnectNtfn:
+		err := idx.disconnectBlock(dbTx, ntfn.Block, ntfn.Parent,
+			ntfn.PrevScripts, ntfn.IsTreasuryEnabled)
+		if err != nil {
+			log.Errorf("%s: unable to disconnect block: %v", idx.Name(), err)
+		}
+
+	default:
+		return fmt.Errorf("%s: unknown notification type received: %d",
+			idx.Name(), ntfn.NtfnType)
+	}
+
+	return nil
 }
