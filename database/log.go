0deef709df60a7db4a7d9b4a8da9dae0e2ca3374
Dave Collins <davec@conformal.com>
2021-09-04 18:59:10 -0500

    database: Remove deprecated DisableLog.

--- database/log.go
+++ database/log.go
@@ -9,24 +9,8 @@ import (
 	"github.com/decred/slog"
 )
 
-// log is a logger that is initialized with no output filters.  This
-// means the package will not perform any logging by default until the caller
-// requests it.
-// The default amount of logging is none.
-var log = slog.Disabled
-
-// DisableLog disables all library log output.  Logging output is disabled
-// by default until UseLogger is called.
-//
-// Deprecated: Use UseLogger(slog.Disabled) instead.
-func DisableLog() {
-	log = slog.Disabled
-}
-
 // UseLogger uses a specified Logger to output package logging info.
 func UseLogger(logger slog.Logger) {
-	log = logger
-
 	// Update the logger for the registered drivers.
 	for _, drv := range drivers {
 		if drv.UseLogger != nil {
