bd94ed5ee981db0cf5be6ed9ce6fc0abb8e1b65c
Dave Collins <davec@conformal.com>
2021-09-04 19:24:26 -0500

    secp256k1: Prepare v4.0.0.
    This updates the secp256k1 copyright year in the files modified since
    the previous release and serves as a base for dcrec/secp256k1/v4.0.0.
    
    There are no updated direct dependencies in this commit.
    
    The following direct dependencies are no longer required as compared to
    the previous dcrec/secp256k1/v3.0.0 release:
    
    - github.com/decred/dcrd/chaincfg/chainhash

--- dcrec/secp256k1/curve_test.go
+++ dcrec/secp256k1/curve_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2020 The Decred developers
+// Copyright (c) 2015-2021 The Decred developers
 // Copyright 2013-2016 The btcsuite developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
--- dcrec/secp256k1/field.go
+++ dcrec/secp256k1/field.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2013-2014 The btcsuite developers
-// Copyright (c) 2015-2020 The Decred developers
-// Copyright (c) 2013-2020 Dave Collins
+// Copyright (c) 2015-2021 The Decred developers
+// Copyright (c) 2013-2021 Dave Collins
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
