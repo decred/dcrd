3a8c676a9ebbf024ff67c20f85a4af18c0ddb974
Ryan Staudt <mail@ryanstaudt.com>
2021-09-22 13:51:26 -0500

    mining: Update to latest block vers for HFV.
    This updates the block versions created by the mining code to produce
    the latest versions needed to trigger the hard fork vote for DCP0007,
    DCP0008, and DCP0009.

--- internal/mining/mining.go
+++ internal/mining/mining.go
@@ -258,11 +258,11 @@ const (
 	// will require changes to the generated block.  Using the wire constant
 	// for generated block version could allow creation of invalid blocks
 	// for the updated version.
-	generatedBlockVersion = 8
+	generatedBlockVersion = 9
 
 	// generatedBlockVersionTest is the version of the block being generated
 	// for networks other than the main and simulation networks.
-	generatedBlockVersionTest = 9
+	generatedBlockVersionTest = 10
 
 	// blockHeaderOverhead is the max number of bytes it takes to serialize
 	// a block header and max possible transaction count.
