e785c25a68319fe42376c1c8c275fe885827fdbd
Dave Collins <davec@conformal.com>
2021-11-06 16:06:38 -0500

    uint256: Add big endian set benchmarks.
    The following is a comparison between stdlib big integers (old) and the
    specialized type (new) averaging 10 runs each:
    
    name       old time/op    new time/op    delta
    ------------------------------------------------------------------
    SetBytes   9.09ns ±13%    3.05ns ± 1%   -66.43%  (p=0.000 n=10+10)
    
    name       old allocs/op  new allocs/op  delta
    ----------------------------------------------------------
    SetBytes   0.00           0.00           ~     (all equal)
    
    This is part of a series of commits to fully implement the uint256
    package.

---
+++ internal/staging/primitives/uint256/uint256_bench_test.go
@@ -0,0 +1,82 @@
+// Copyright (c) 2021 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package uint256
+
+import (
+	"fmt"
+	"math/big"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+// randBenchVal houses values used throughout the benchmarks that are randomly
+// generated with each run to ensure they are not overfitted.
+type randBenchVal struct {
+	buf1  [32]byte
+	buf2  [32]byte
+	n1    *Uint256
+	n2    *Uint256
+	bigN1 *big.Int
+	bigN2 *big.Int
+}
+
+// randBenchVals houses a slice of the aforementioned randomly-generated values
+// to be used throughout the benchmarks to ensure they are not overfitted.
+var randBenchVals = func() []randBenchVal {
+	// Use a unique random seed each benchmark instance.
+	seed := time.Now().Unix()
+	rng := rand.New(rand.NewSource(seed))
+	var zeroArr [32]byte
+
+	vals := make([]randBenchVal, 512)
+	for i := 0; i < len(vals); i++ {
+		val := &vals[i]
+		if _, err := rng.Read(val.buf1[:]); err != nil {
+			panic(fmt.Sprintf("failed to read random: %v", err))
+		}
+		for val.buf2 == zeroArr {
+			if _, err := rng.Read(val.buf2[:]); err != nil {
+				panic(fmt.Sprintf("failed to read random: %v", err))
+			}
+		}
+
+		val.n1 = new(Uint256).SetBytes(&val.buf1)
+		val.n2 = new(Uint256).SetBytes(&val.buf2)
+		val.bigN1 = new(big.Int).SetBytes(val.buf1[:])
+		val.bigN2 = new(big.Int).SetBytes(val.buf2[:])
+	}
+	return vals
+}()
+
+// BenchmarkUint256SetBytes benchmarks initializing an unsigned 256-bit integer
+// from bytes in big endian with the specialized type.
+func BenchmarkUint256SetBytes(b *testing.B) {
+	n := new(Uint256)
+	vals := randBenchVals
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i += len(vals) {
+		for j := 0; j < len(vals); j++ {
+			n.SetBytes(&vals[j].buf1)
+		}
+	}
+}
+
+// BenchmarkBigIntSetBytes benchmarks initializing an unsigned 256-bit integer
+// from bytes in big endian with stdlib big integers.
+func BenchmarkBigIntSetBytes(b *testing.B) {
+	n := new(big.Int)
+	vals := randBenchVals
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i += len(vals) {
+		for j := 0; j < len(vals); j++ {
+			n.SetBytes(vals[j].buf1[:])
+		}
+	}
+}
