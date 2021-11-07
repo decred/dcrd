3e7115cf64615e32d3317ee1c865f5d68a1046d9
Dave Collins <davec@conformal.com>
2021-11-07 16:29:26 -0600

    primitives: Add diff bits conversion benchmarks.
    The following is a comparison between the existing implementations in
    the blockchain/standalone module with big integers (old) and the new
    implementations (new) averaging 10 runs each:
    
    name                old time/op     new time/op     delta
    ------------------------------------------------------------------------------
    DiffBitsToUint256   158ns ± 1%      4ns ± 1%        -97.35%  (p=0.000 n=10+10)
    Uint256ToDiffBits   151ns ± 2%      8ns ± 4%        -94.82%  (p=0.000 n=10+10)
    
    name                      old allocs/op   new allocs/op   delta
    -------------------------------------------------------------------------------
    DiffBitsToUint256   3.00 ± 0%       0.00            -100.00%  (p=0.000 n=10+10)
    Uint256ToDiffBits   2.00 ± 0%       0.00            -100.00%  (p=0.000 n=10+10)

---
+++ internal/staging/primitives/pow_bench_test.go
@@ -0,0 +1,32 @@
+// Copyright (c) 2021 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package primitives
+
+import (
+	"testing"
+)
+
+// BenchmarkDiffBitsToUint256 benchmarks converting the compact representation
+// used to encode difficulty targets to an unsigned 256-bit integer.
+func BenchmarkDiffBitsToUint256(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		const input = 0x1b01330e
+		DiffBitsToUint256(input)
+	}
+}
+
+// BenchmarkUint256ToDiffBits benchmarks converting an unsigned 256-bit integer
+// to the compact representation used to encode difficulty targets.
+func BenchmarkUint256ToDiffBits(b *testing.B) {
+	n := hexToUint256("1330e000000000000000000000000000000000000000000000000")
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Uint256ToDiffBits(n)
+	}
+}
