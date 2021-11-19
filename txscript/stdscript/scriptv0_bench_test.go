b1b9a86da0da260d243cdcf014e465c8f6cece86
Dave Collins <davec@conformal.com>
2021-11-18 18:57:36 -0600

    stdscript: Add extract v0 stake-tagged p2sh bench.
    BenchmarkExtractStakeScriptHashV0
    ---------------------------------
    v0_complex_non_standard       226793582   5.253 ns/op
    v0_stake_submission_p2sh      372232682   3.233 ns/op
    v0_stake_gen_p2sh             305602378   3.979 ns/op
    v0_stake_revoke_p2sh          201691552   5.064 ns/op
    v0_stake_change_p2sh          209401243   5.687 ns/op
    v0_treasury_generation_p2sh   221794405   5.419 ns/op

--- txscript/stdscript/scriptv0_bench_test.go
+++ txscript/stdscript/scriptv0_bench_test.go
@@ -34,3 +34,29 @@ func BenchmarkExtractStakePubKeyHashV0(b *testing.B) {
 		})
 	}
 }
+
+// BenchmarkExtractStakeScriptHashV0 benchmarks the performance of attempting to
+// extract script hashes from various version 0 stake-tagged public key scripts.
+func BenchmarkExtractStakeScriptHashV0(b *testing.B) {
+	counts := make(map[ScriptType]int)
+	benches := makeBenchmarks(func(test scriptTest) bool {
+		// Limit to one of each script type.
+		counts[test.wantType]++
+		return counts[test.wantType] == 1 &&
+			(test.wantType == STStakeSubmissionScriptHash ||
+				test.wantType == STStakeGenScriptHash ||
+				test.wantType == STStakeRevocationScriptHash ||
+				test.wantType == STStakeChangeScriptHash ||
+				test.wantType == STTreasuryGenScriptHash)
+	})
+
+	for _, bench := range benches {
+		b.Run(bench.name, func(b *testing.B) {
+			b.ReportAllocs()
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				ExtractStakeScriptHashV0(bench.script)
+			}
+		})
+	}
+}
