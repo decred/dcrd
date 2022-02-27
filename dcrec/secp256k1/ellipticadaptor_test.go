636070e7b416c66518371a6bdee9624a2a1f8105
Dave Collins <davec@conformal.com>
2022-02-27 02:41:33 -0600

    secp256k1: Cleanup affine scalar base mult tests.
    This cleans up the tests for performing affine scalar base
    multiplication to make them more consistent with modern practices in the
    code.
    
    The following is a high level overview of the changes:
    
    - Adds names to each individual test for easier identification
    - Makes the individual test definitions consistent with the rest of the
      code
    - Adds some additional edge cases

--- dcrec/secp256k1/ellipticadaptor_test.go
+++ dcrec/secp256k1/ellipticadaptor_test.go
@@ -5,7 +5,6 @@
 package secp256k1
 
 import (
-	"fmt"
 	"math/big"
 	"testing"
 )
@@ -185,46 +184,72 @@ func TestDoubleAffineAdaptor(t *testing.T) {
 }
 
 // TestScalarBaseMultAdaptor ensures the ScalarBaseMult method used to satisfy
-// the elliptic.Curve interface works as intended.
+// the elliptic.Curve interface works as intended for some edge cases and known
+// good values.
 func TestScalarBaseMultAdaptor(t *testing.T) {
 	tests := []struct {
-		k    string
-		x, y string
+		name   string // test description
+		k      string // hex encoded scalar
+		rx, ry string // hex encoded coordinates of expected point
 	}{{
-		"aa5e28d6a97a2479a65527f7290311a3624d4cc0fa1578598ee3c2613bf99522",
-		"34f9460f0e4f08393d192b3c5133a6ba099aa0ad9fd54ebccfacdfa239ff49c6",
-		"b71ea9bd730fd8923f6d25a7a91e7dd7728a960686cb5a901bb419e0f2ca232",
+		name: "zero",
+		k:    "0000000000000000000000000000000000000000000000000000000000000000",
+		rx:   "0000000000000000000000000000000000000000000000000000000000000000",
+		ry:   "0000000000000000000000000000000000000000000000000000000000000000",
 	}, {
-		"7e2b897b8cebc6361663ad410835639826d590f393d90a9538881735256dfae3",
-		"d74bf844b0862475103d96a611cf2d898447e288d34b360bc885cb8ce7c00575",
-		"131c670d414c4546b88ac3ff664611b1c38ceb1c21d76369d7a7a0969d61d97d",
+		name: "one (aka 1*G = G)",
+		k:    "0000000000000000000000000000000000000000000000000000000000000001",
+		rx:   "79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798",
+		ry:   "483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8",
 	}, {
-		"6461e6df0fe7dfd05329f41bf771b86578143d4dd1f7866fb4ca7e97c5fa945d",
-		"e8aecc370aedd953483719a116711963ce201ac3eb21d3f3257bb48668c6a72f",
-		"c25caf2f0eba1ddb2f0f3f47866299ef907867b7d27e95b3873bf98397b24ee1",
+		name: "group order - 1 (aka -1*G = -G)",
+		k:    "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364140",
+		rx:   "79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798",
+		ry:   "b7c52588d95c3b9aa25b0403f1eef75702e84bb7597aabe663b82f6f04ef2777",
 	}, {
-		"376a3a2cdcd12581efff13ee4ad44c4044b8a0524c42422a7e1e181e4deeccec",
-		"14890e61fcd4b0bd92e5b36c81372ca6fed471ef3aa60a3e415ee4fe987daba1",
-		"297b858d9f752ab42d3bca67ee0eb6dcd1c2b7b0dbe23397e66adc272263f982",
+		name: "known good point 1",
+		k:    "aa5e28d6a97a2479a65527f7290311a3624d4cc0fa1578598ee3c2613bf99522",
+		rx:   "34f9460f0e4f08393d192b3c5133a6ba099aa0ad9fd54ebccfacdfa239ff49c6",
+		ry:   "0b71ea9bd730fd8923f6d25a7a91e7dd7728a960686cb5a901bb419e0f2ca232",
 	}, {
-		"1b22644a7be026548810c378d0b2994eefa6d2b9881803cb02ceff865287d1b9",
-		"f73c65ead01c5126f28f442d087689bfa08e12763e0cec1d35b01751fd735ed3",
-		"f449a8376906482a84ed01479bd18882b919c140d638307f0c0934ba12590bde",
+		name: "known good point 2",
+		k:    "7e2b897b8cebc6361663ad410835639826d590f393d90a9538881735256dfae3",
+		rx:   "d74bf844b0862475103d96a611cf2d898447e288d34b360bc885cb8ce7c00575",
+		ry:   "131c670d414c4546b88ac3ff664611b1c38ceb1c21d76369d7a7a0969d61d97d",
+	}, {
+		name: "known good point 3",
+		k:    "6461e6df0fe7dfd05329f41bf771b86578143d4dd1f7866fb4ca7e97c5fa945d",
+		rx:   "e8aecc370aedd953483719a116711963ce201ac3eb21d3f3257bb48668c6a72f",
+		ry:   "c25caf2f0eba1ddb2f0f3f47866299ef907867b7d27e95b3873bf98397b24ee1",
+	}, {
+		name: "known good point 4",
+		k:    "376a3a2cdcd12581efff13ee4ad44c4044b8a0524c42422a7e1e181e4deeccec",
+		rx:   "14890e61fcd4b0bd92e5b36c81372ca6fed471ef3aa60a3e415ee4fe987daba1",
+		ry:   "297b858d9f752ab42d3bca67ee0eb6dcd1c2b7b0dbe23397e66adc272263f982",
+	}, {
+		name: "known good point 5",
+		k:    "1b22644a7be026548810c378d0b2994eefa6d2b9881803cb02ceff865287d1b9",
+		rx:   "f73c65ead01c5126f28f442d087689bfa08e12763e0cec1d35b01751fd735ed3",
+		ry:   "f449a8376906482a84ed01479bd18882b919c140d638307f0c0934ba12590bde",
 	}}
 
-	s256 := S256()
-	for i, test := range tests {
-		k, ok := new(big.Int).SetString(test.k, 16)
-		if !ok {
-			t.Errorf("%d: bad value for k: %s", i, test.k)
+	curve := S256()
+	for _, test := range tests {
+		// Parse the test data.
+		k := fromHex(test.k)
+		xWant, yWant := fromHex(test.rx), fromHex(test.ry)
+
+		// Ensure the test data is using points that are actually on the curve
+		// (or the point at infinity).
+		if !isValidAffinePoint(xWant, yWant) {
+			t.Errorf("%s: expected point is not on curve", test.name)
+			continue
 		}
-		x, y := s256.ScalarBaseMult(k.Bytes())
-		if fmt.Sprintf("%x", x) != test.x || fmt.Sprintf("%x", y) != test.y {
-			t.Errorf("%d: bad output for k=%s: got (%x, %x), want (%s, %s)", i,
-				test.k, x, y, test.x, test.y)
-		}
-		if testing.Short() && i > 5 {
-			break
+
+		rx, ry := curve.ScalarBaseMult(k.Bytes())
+		if rx.Cmp(xWant) != 0 || ry.Cmp(yWant) != 0 {
+			t.Errorf("%s: wrong result:\ngot (%x, %x)\nwant (%x, %x)",
+				test.name, rx, ry, xWant, yWant)
 		}
 	}
 }
