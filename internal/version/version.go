db49746b9a11433153d4c3c4111e00e978a04452
Dave Collins <davec@conformal.com>
2022-05-04 00:31:22 -0500

    [release-v1.7] release: Bump for 1.7.2.

--- internal/version/version.go
+++ internal/version/version.go
@@ -56,7 +56,7 @@ var (
 	// the package will panic at runtime.  Of particular note is the pre-release
 	// and build metadata portions MUST only contain characters from
 	// semanticAlphabet.
-	Version = "1.7.1+release.local"
+	Version = "1.7.2+release.local"
 
 	// NOTE: The following values are set via init by parsing the above Version
 	// string.
