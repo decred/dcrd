1629418c7e0f018da484e468e21cf04850fcf3a4
Dave Collins <davec@conformal.com>
2022-06-03 17:16:40 -0500

    dcrd: Support SIGTERM on Win and all unix variants.
    Go 1.14 added runtime support for handling the windows
    CTRL_CLOSE_EVENT, CTRL_LOGOFF_EVENT, and CTRL_SHUTDOWN_EVENT events by
    adding a definition for syscall.SIGTERM to windows and converting the
    events to that signal.  It also added definitions for other common
    signals, including SIGHUP, and treats them as NOOPs on windows.
    
    Consequently, this modifies the logic that deals with conditionally
    handling SIGTERM and SIGHUP to handle them for windows as well as all
    unix-like operating systems, including newer ones that are supported by
    Go since the code was originally written.
    
    Although the additional signals could probably just be added
    unconditionally without too much issue now due to the runtime adding
    stubs to all operating systems the project officially supports, it is
    safer to be explicit when dealing with syscall definitions since there
    is no guarantee that they will exist for newly-added operating systems.
    
    Stated more plainly, this change means dcrd will now be shutdown cleanly
    on more variants of unix as well as when being terminated by windows
    itself due to various things such as the user logging off, the window
    terminal being closed, and the system shutting down.

---
+++ signal_syscall.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2021-2022 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+//
+//go:build windows || aix || android || darwin || dragonfly || freebsd || hurd || illumos || ios || linux || netbsd || openbsd || solaris
+// +build windows aix android darwin dragonfly freebsd hurd illumos ios linux netbsd openbsd solaris
+
+package main
+
+import (
+	"syscall"
+)
+
+func init() {
+	interruptSignals = append(interruptSignals, syscall.SIGTERM, syscall.SIGHUP)
+}
