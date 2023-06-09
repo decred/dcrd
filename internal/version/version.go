84df48626a9a91f70c563a5c7655b66e233b1826
Dave Collins <davec@conformal.com>
2023-06-09 11:02:30 -0500

    [release-v1.8] release: Bump for 1.8.0.
    This clears the PreRelease and sets the BuildMetadata to release.local
    on the release branch so that anyone building the release branch will
    end up with version "1.8.0+release.local" indicating it was a local
    build as opposed to a reproducible release build.

--- internal/version/version.go
+++ internal/version/version.go
@@ -56,7 +56,7 @@ var (
 	// the package will panic at runtime.  Of particular note is the pre-release
 	// and build metadata portions MUST only contain characters from
 	// semanticAlphabet.
-	Version = "1.8.0-pre"
+	Version = "1.8.0+release.local"
 
 	// NOTE: The following values are set via init by parsing the above Version
 	// string.
