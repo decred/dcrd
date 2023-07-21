a40b79393bdaedd2d9eb9f759b1ecec66bb8a7bf
Dave Collins <davec@conformal.com>
2023-07-20 19:00:29 -0500

    build: Use golangci action and make parallel.
    This updates the GitHub workflow to use the official golangci-lint
    action which has additional caching logic and also creates GitHub
    annotations for any issues which makes it easier to identify issues
    versus needing to dig through build logs.
    
    It also arranges for the linters to run as a separate job which executes
    in parallel to the go build and tests.
    
    Since the repository has multiple modules and golangci-lint does not
    support running all modules in the repository directly, this uses a
    separate job to resolve the modules dynamically and then feeds those
    into the separate linter job as a matrix of modules (work directories)
    so the linters run on all modules in the repo.
    
    In order to avoid running the linters as a part of the normal test job
    while still allowing developers to invoke all of the tests and linters
    via the "run_tests.sh" script, this separates the linter logic into a
    separate "lint.sh" script and conditionally invokes that script only
    when not running as a GitHub action.
    
    Finally, it pins the dependencies for both the action itself as well as
    the golangci-lint version by using their respective hashes.

--- run_tests.sh
+++ run_tests.sh
@@ -2,17 +2,11 @@
 
 set -ex
 
-# The script does automatic checking on a Go package and its sub-packages,
-# including:
-# 1. gofmt         (https://golang.org/cmd/gofmt/)
-# 2. gosimple      (https://github.com/dominikh/go-simple)
-# 3. unconvert     (https://github.com/mdempsky/unconvert)
-# 4. ineffassign   (https://github.com/gordonklaus/ineffassign)
-# 5. go vet        (https://golang.org/cmd/vet)
-# 6. misspell      (https://github.com/client9/misspell)
-
-# golangci-lint (github.com/golangci/golangci-lint) is used to run each
-# static checker.
+# This script runs the tests for all packages in all Go modules in the
+# repository.
+#
+# It will also run the linters for all Go modules in the repository when not
+# running as a GitHub action.
 
 go version
 
@@ -21,27 +15,7 @@ echo "==> test all modules"
 ROOTPKG=$(go list)
 go test -short -tags rpctest $ROOTPKG/...
 
-# loop all modules
-ROOTPKGPATTERN=$(echo $ROOTPKG | sed 's,\\,\\\\,g' | sed 's,/,\\/,g')
-MODPATHS=$(go list -m all | grep "^$ROOTPKGPATTERN" | cut -d' ' -f1)
-for module in $MODPATHS; do
-  echo "==> lint ${module}"
-
-  # determine module directory
-  MODNAME=$(echo $module | sed -E -e "s/^$ROOTPKGPATTERN//" \
-    -e 's,^/,,' -e 's,/v[0-9]+$,,')
-  if [ -z "$MODNAME" ]; then
-    MODNAME=.
-  fi
-
-  # run commands in the module directory as a subshell
-  (
-    cd $MODNAME
-
-    # run linters
-    golangci-lint run
-  )
-done
+[ -z "$GITHUB_ACTIONS" ] && ./lint.sh
 
 echo "------------------------------------------"
 echo "Tests completed successfully!"
