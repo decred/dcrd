db7785f8f66f89052a2268d5099db7b9e3cfe9b7
Josh Rickmar <jrick@companyzero.com>
2024-05-03 20:33:35 +0000

    wire: Remove PR hashes from (get)initstate
    This reverts all of the initstate changes made in
    b9d8d49c901bb7cbb19ed36d636c3e3d86a1fe43.
    
    The limit of 100 was too too low to be useful and cranking this value up was
    not a proper solution either.  It also only synced pair request messages,
    which would cause mid-run mixes to leave stale PRs around in the mixpool until
    they expired.
    
    This is handled better in newer mixpool code by requesting unknown pair
    request messages when they are seen in a key exchange.  The mixpool records
    the KE as an orphan and will process it after all missing PRs have been
    requested and processed.

--- wire/msginitstate.go
+++ wire/msginitstate.go
@@ -22,10 +22,6 @@ const MaxISVotesAtHeadPerMsg = 40 // 8 * 5
 // message.
 const MaxISTSpendsAtHeadPerMsg = 7
 
-// MaxISMixPairReqsPerMsg is the maximum number of pair request mix messages
-// at head per message.
-const MaxISMixPairReqsPerMsg = 100
-
 // MsgInitState implements the Message interface and represents an initial
 // state message.  It is used to receive ephemeral startup information from a
 // remote peer, such as blocks that can be mined upon, votes for such blocks
@@ -34,10 +30,9 @@ const MaxISMixPairReqsPerMsg = 100
 // The content of such a message depends upon what the local peer requested
 // during a previous GetInitState msg.
 type MsgInitState struct {
-	BlockHashes      []chainhash.Hash
-	VoteHashes       []chainhash.Hash
-	TSpendHashes     []chainhash.Hash
-	MixPairReqHashes []chainhash.Hash
+	BlockHashes  []chainhash.Hash
+	VoteHashes   []chainhash.Hash
+	TSpendHashes []chainhash.Hash
 }
 
 // AddBlockHash adds a new block hash to the message. Up to
@@ -79,20 +74,6 @@ func (msg *MsgInitState) AddTSpendHash(hash *chainhash.Hash) error {
 	}
 
 	msg.TSpendHashes = append(msg.TSpendHashes, *hash)
-	return nil
-}
-
-// AddMixPairReqHash adds a new mixing pair request message hash to the message.
-// Up to MaxISMixPRsPerMsg may be added before this function errors out.
-func (msg *MsgInitState) AddMixPairReqHash(hash *chainhash.Hash) error {
-	const op = "MsgInitState.AddMixPairReqHash"
-	if len(msg.MixPairReqHashes)+1 > MaxISMixPairReqsPerMsg {
-		msg := fmt.Sprintf("too many mixpairreq hashes for message [max %v]",
-			MaxISMixPairReqsPerMsg)
-		return messageError(op, ErrTooManyManyMixPairReqs, msg)
-	}
-
-	msg.MixPairReqHashes = append(msg.MixPairReqHashes, *hash)
 	return nil
 }
 
@@ -158,29 +139,6 @@ func (msg *MsgInitState) BtcDecode(r io.Reader, pver uint32) error {
 	msg.TSpendHashes = make([]chainhash.Hash, count)
 	for i := uint64(0); i < count; i++ {
 		err := readElement(r, &msg.TSpendHashes[i])
-		if err != nil {
-			return err
-		}
-	}
-
-	// Read num mixpairreq hashes (when enabled by protocol).
-	if pver < MixVersion {
-		return nil
-	}
-
-	count, err = ReadVarInt(r, pver)
-	if err != nil {
-		return err
-	}
-	if count > MaxISMixPairReqsPerMsg {
-		msg := fmt.Sprintf("too many mixpairreq hashes for message "+
-			"[count %v, max %v]", count, MaxISMixPairReqsPerMsg)
-		return messageError(op, ErrTooManyManyMixPairReqs, msg)
-	}
-
-	msg.MixPairReqHashes = make([]chainhash.Hash, count)
-	for i := uint64(0); i < count; i++ {
-		err := readElement(r, &msg.MixPairReqHashes[i])
 		if err != nil {
 			return err
 		}
@@ -259,30 +217,6 @@ func (msg *MsgInitState) BtcEncode(w io.Writer, pver uint32) error {
 		}
 	}
 
-	// Write mixpairreq hashes when enabled by protocol.
-	if pver < MixVersion {
-		return nil
-	}
-
-	count = len(msg.MixPairReqHashes)
-	if count > MaxISMixPairReqsPerMsg {
-		msg := fmt.Sprintf("too many mixpairreq hashes for message "+
-			"[count %v, max %v]", count, MaxISMixPairReqsPerMsg)
-		return messageError(op, ErrTooManyManyMixPairReqs, msg)
-	}
-
-	err = WriteVarInt(w, pver, uint64(count))
-	if err != nil {
-		return err
-	}
-
-	for i := range msg.MixPairReqHashes {
-		err = writeElement(w, &msg.MixPairReqHashes[i])
-		if err != nil {
-			return err
-		}
-	}
-
 	return nil
 }
 
@@ -299,29 +233,21 @@ func (msg *MsgInitState) MaxPayloadLength(pver uint32) uint32 {
 		return 0
 	}
 
-	max := uint32(VarIntSerializeSize(MaxISBlocksAtHeadPerMsg)) +
+	return uint32(VarIntSerializeSize(MaxISBlocksAtHeadPerMsg)) +
 		(MaxISBlocksAtHeadPerMsg * chainhash.HashSize) +
 		uint32(VarIntSerializeSize(MaxISVotesAtHeadPerMsg)) +
 		(MaxISVotesAtHeadPerMsg * chainhash.HashSize) +
 		uint32(VarIntSerializeSize(MaxISTSpendsAtHeadPerMsg)) +
 		(MaxISTSpendsAtHeadPerMsg * chainhash.HashSize)
-
-	if pver >= MixVersion {
-		max += uint32(VarIntSerializeSize(MaxISMixPairReqsPerMsg)) +
-			(MaxISMixPairReqsPerMsg * chainhash.HashSize)
-	}
-
-	return max
 }
 
 // NewMsgInitState returns a new Decred initstate message that conforms to the
 // Message interface using the defaults for the fields.
 func NewMsgInitState() *MsgInitState {
 	return &MsgInitState{
-		BlockHashes:      make([]chainhash.Hash, 0, MaxISBlocksAtHeadPerMsg),
-		VoteHashes:       make([]chainhash.Hash, 0, MaxISVotesAtHeadPerMsg),
-		TSpendHashes:     make([]chainhash.Hash, 0, MaxISTSpendsAtHeadPerMsg),
-		MixPairReqHashes: make([]chainhash.Hash, 0, MaxISMixPairReqsPerMsg),
+		BlockHashes:  make([]chainhash.Hash, 0, MaxISBlocksAtHeadPerMsg),
+		VoteHashes:   make([]chainhash.Hash, 0, MaxISVotesAtHeadPerMsg),
+		TSpendHashes: make([]chainhash.Hash, 0, MaxISTSpendsAtHeadPerMsg),
 	}
 }
 
@@ -356,17 +282,9 @@ func NewMsgInitStateFilled(blockHashes []chainhash.Hash, voteHashes []chainhash.
 		return nil, messageError(op, ErrTooManyTSpends, msg)
 	}
 
-	// Not preallocated.  The purpose of this function is to create a
-	// MsgInitState from already allocated data.  Assume that if the
-	// caller also has mixpairreq hashes to include, they have also been
-	// preallocated, so there is no reason to allocate the memory here
-	// too.
-	var mixPairReqHashes []chainhash.Hash
-
 	return &MsgInitState{
-		BlockHashes:      blockHashes,
-		VoteHashes:       voteHashes,
-		TSpendHashes:     tspendHashes,
-		MixPairReqHashes: mixPairReqHashes,
+		BlockHashes:  blockHashes,
+		VoteHashes:   voteHashes,
+		TSpendHashes: tspendHashes,
 	}, nil
 }
