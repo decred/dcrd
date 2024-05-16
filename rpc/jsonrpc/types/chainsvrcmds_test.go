e24925a8fe8e9b708ba32a0aa66d1660fc64b3ca
Dave Collins <davec@conformal.com>
2024-05-15 20:23:33 -0500

    rpc/jsonrpc/types: Prepare v4.2.0.
    This updates the rpc/jsonrpc/types module copyright year in the files
    modified since the previous release and serves as a base for
    rpc/jsonrpc/types/v4.2.0.

--- rpc/jsonrpc/types/chainsvrcmds_test.go
+++ rpc/jsonrpc/types/chainsvrcmds_test.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2014 The btcsuite developers
-// Copyright (c) 2016-2022 The Decred developers
+// Copyright (c) 2016-2024 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
