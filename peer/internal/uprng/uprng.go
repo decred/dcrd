55596c2420517f868c38d573005905e25ca61771
Josh Rickmar <jrick@companyzero.com>
2024-06-12 22:23:02 +0000

    peer: Randomize inv batching delays
    This changes the inventory batching delay from a static 500ms ticker to a
    random delay uniformly distributed between 100-500ms.  The intention is to
    improve privacy while simultaneously increasing the network propagation speed
    of inventoried messages (including transactions, blocks, and mixing messages)
    and continuing to keep inventory batching useful and occurring when many
    messages are inventoried in the same short duration.
    
    As this change rolls out to more nodes on the network, this will cause not
    only more random jitter into the sending and timing of messages, but also
    change the message paths.  Currently, with every peer using a 500ms ticker, if
    no changes occur to the graph of connected nodes, messages will always
    propagate the nodes in the same order.  However, if all nodes are randomizing
    their inventory batching delays, the next peer who will relay a message first
    during the next hop will always be different.

---
+++ peer/internal/uprng/uprng.go
@@ -0,0 +1,119 @@
+// Copyright (c) 2024 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package uprng
+
+import (
+	cryptorand "crypto/rand"
+	"encoding/binary"
+	"io"
+	"math/bits"
+	"sync"
+	"time"
+
+	"golang.org/x/crypto/chacha20"
+)
+
+// Reader implements a cryptographically secure userspace PRNG that is
+// periodically reseeded with entropy obtained from crypto/rand.
+// Reader is safe for concurrent access.
+var Reader io.Reader
+
+// Read fills b with random bytes obtained from the userspace PRNG.
+//
+// When possible, prefer Read over using Reader to avoid escape analysis
+// unnecessarily allocating b's backing array on the heap.
+func Read(b []byte) {
+	defaultReader.Read(b)
+}
+
+var defaultReader = newPRNG()
+
+func init() {
+	Reader = defaultReader
+}
+
+const (
+	maxCipherRead     = 4 * 1024 * 1024 // 4 MiB
+	maxCipherDuration = 20 * time.Second
+)
+
+// nonce implements a 12-byte little endian counter suitable for use as an
+// incrementing ChaCha20 nonce.
+type nonce [chacha20.NonceSize]byte
+
+func (n *nonce) inc() {
+	n0 := binary.LittleEndian.Uint32(n[0:4])
+	n1 := binary.LittleEndian.Uint32(n[4:8])
+	n2 := binary.LittleEndian.Uint32(n[8:12])
+
+	var carry uint32
+	n0, carry = bits.Add32(n0, 1, carry)
+	n1, carry = bits.Add32(n1, 0, carry)
+	n2, carry = bits.Add32(n2, 0, carry)
+	n0, _ = bits.Add32(n0, 0, carry)
+
+	binary.LittleEndian.PutUint32(n[0:4], n0)
+	binary.LittleEndian.PutUint32(n[4:8], n1)
+	binary.LittleEndian.PutUint32(n[8:12], n2)
+}
+
+type prng struct {
+	cipher *chacha20.Cipher
+	read   int
+	t      time.Time
+	key    []byte
+	nonce  nonce
+	mu     sync.Mutex
+}
+
+func newPRNG() *prng {
+	p := &prng{
+		key: make([]byte, chacha20.KeySize),
+	}
+	p.seed()
+	return p
+}
+
+// seed reseeds the prng with kernel and existing cipher entropy, if the
+// cipher has been originally seeded.
+// Panics only during initial seeding if a crypto/rand read errors.
+func (p *prng) seed() {
+	_, err := cryptorand.Read(p.key)
+	if err != nil && p.cipher == nil {
+		panic(err)
+	}
+	if p.cipher != nil {
+		p.cipher.XORKeyStream(p.key, p.key)
+	}
+
+	// never errors with correct key and nonce sizes
+	cipher, _ := chacha20.NewUnauthenticatedCipher(p.key, p.nonce[:])
+	p.nonce.inc()
+
+	p.cipher = cipher
+	p.read = 0
+	p.t = time.Now().Add(maxCipherDuration)
+}
+
+func (p *prng) Read(s []byte) (n int, err error) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if time.Now().After(p.t) {
+		p.seed()
+	}
+
+	for p.read+len(s) > maxCipherRead {
+		l := maxCipherRead - p.read
+		p.cipher.XORKeyStream(s[:l], s[:l])
+		p.seed()
+		n += l
+		s = s[l:]
+	}
+	p.cipher.XORKeyStream(s, s)
+	p.read += len(s)
+	n += len(s)
+	return
+}
