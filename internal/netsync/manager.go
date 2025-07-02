01e67bd6525dc65dfe87ec4cce709de887eed4ad
Dave Collins <davec@conformal.com>
2025-07-02 03:46:15 -0500

    netsync: Remove unused msg channel.
    This removes the event handler message channel since it is no longer
    used.
    
    This is a part of the overall effort to convert the code related to
    handling the various sync manager events to synchronous code that runs
    in the various caller goroutines.

--- internal/netsync/manager.go
+++ internal/netsync/manager.go
@@ -304,7 +304,6 @@ type SyncManager struct {
 	rejectedTxns    *apbf.Filter
 	rejectedMixMsgs *apbf.Filter
 	progressLogger  *progresslog.Logger
-	msgChan         chan interface{}
 
 	// These fields track pending requests for data from all peers.  They are
 	// protected by the request mutex.
@@ -1889,13 +1888,6 @@ func (m *SyncManager) eventHandler(ctx context.Context) {
 out:
 	for {
 		select {
-		case data := <-m.msgChan:
-			switch msg := data.(type) {
-
-			default:
-				log.Warnf("Invalid message type in event handler: %T", msg)
-			}
-
 		case <-m.hdrSyncState.stallTimer.C:
 			// Disconnect the sync peer due to stalling the header sync process.
 			m.syncPeerMtx.Lock()
@@ -2208,7 +2200,6 @@ func New(config *Config) *SyncManager {
 		minKnownWork:     minKnownWork,
 		hdrSyncState:     makeHeaderSyncState(),
 		progressLogger:   progresslog.New("Processed", log),
-		msgChan:          make(chan interface{}, config.MaxPeers*3),
 		quit:             make(chan struct{}),
 	}
 	mgr.syncHeight.Store(config.Chain.BestSnapshot().Height)
