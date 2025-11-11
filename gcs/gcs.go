967be8a52b6bc67e341b06a466de1540e14cfbce
Dave Collins <davec@conformal.com>
2025-11-10 23:17:42 -0600

    gcs: Remove old conditional Go 1.12 code.
    Since the module requires a minimum of Go 1.17 now, there is no longer
    any reason to keep the conditional compilation of the fast reduction
    logic on Go versions prior to 1.12 given it can't build on them anymore
    anyway.
    
    With that in mind, this moves the conditionally-compiled implementation
    for Go1.13+ into the main file so that it is always compiled as the only
    implementation and removes the old conditionally-compiled implementation
    for Go versions prior to 1.12.

--- gcs/gcs.go
+++ gcs/gcs.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2016-2017 The btcsuite developers
 // Copyright (c) 2016-2017 The Lightning Network Developers
-// Copyright (c) 2018-2023 The Decred developers
+// Copyright (c) 2018-2025 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
@@ -11,6 +11,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"math"
+	"math/bits"
 	"sort"
 	"sync"
 
@@ -24,6 +25,34 @@ import (
 // consists of x mod N.
 func modReduceV1(x, N uint64) uint64 {
 	return x % N
+}
+
+// fastReduce calculates a mapping that is more or less equivalent to x mod N.
+// However, instead of using a mod operation that can lead to slowness on many
+// processors when not using a power of two due to unnecessary division, this
+// uses a "multiply-and-shift" trick that eliminates all divisions as described
+// in a blog post by Daniel Lemire, located at the following site at the time
+// of this writing:
+// https://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction/
+//
+// Since that link might disappear, the general idea is to multiply by N and
+// shift right by log2(N).  Since N is a 64-bit integer in this case, it
+// becomes:
+//
+// (x * N) / 2^64 == (x * N) >> 64
+//
+// This is a fair map since it maps integers in the range [0,2^64) to multiples
+// of N in [0, N*2^64) and then divides by 2^64 to map all multiples of N in
+// [0,2^64) to 0, all multiples of N in [2^64, 2*2^64) to 1, etc.  This results
+// in either ceil(2^64/N) or floor(2^64/N) multiples of N.
+func fastReduce(x, N uint64) uint64 {
+	// This uses math/bits to perform the 128-bit multiplication as the compiler
+	// will replace it with the relevant intrinsic on most architectures.
+	//
+	// The high 64 bits in a 128-bit product is the same as shifting the entire
+	// product right by 64 bits.
+	hi, _ := bits.Mul64(x, N)
+	return hi
 }
 
 // chooseReduceFunc chooses which reduction method to use based on the filter
