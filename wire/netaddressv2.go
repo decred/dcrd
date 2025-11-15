b0af18c297caf4b6e2898bff721f7e159c7b3eed
Sef Boukenken <sefbkn@users.noreply.github.com>
2025-11-15 14:36:00 -0500

    wire: Add addrv2 message
    This adds the addrv2 message type to allow extensible peer address
    exchange. It also defines the protocol version AddrV2Version (12)
    for future negotiations, but does not advertise it on the peer to
    peer network or use it outside of the wire package.
    
    The structure of the addrv2 message is depicted below for reference.
    
    MsgAddrV2 wire format:
    +---------------------------+
    | Count (varint, [1..1000]) |
    +---------------------------+
    | NetAddressV2[N]           |
    | (see below)               |
    +---------------------------+
    | ...                       |
    +---------------------------+
    
    The varint Count prefix of the MsgAddrV2 payload describes how many
    NetAddressV2 entries exist in the message. This value must indicate
    at least 1 entry and no more than 1000 entries to be considered a
    valid message.  This differs from the addr message in that it does
    not allow zero entries.
    
    NetAddressV2 wire format:
    +-----------+----------+--------+----------------+---------+
    | Timestamp | Services |  Type  |     Address    |  Port   |
    +-----------+----------+--------+----------------+---------+
    |  8 bytes  | 8 bytes  | 1 byte | variable bytes | 2 bytes |
    +-----------+----------+--------+----------------+---------+
    
    The Type field of the NetAddressV2 payload informs the reader
    of both the network type that the address belongs to as well as
    allowing the reader to infer how many bytes to read from the
    address field.
    
    The addr message is also modified to now fail when encoding/decoding
    when the negotiated protocol version >= AddrV2Version.
    
    The following is an overview of the changes:
    
    - Add NetAddressV2 type
      - This includes a constructor NewNetAddressV2 that will be
        needed in a future commit for directly constructing
        v2 wire network addresses with varying types.
    - Add MsgAddrV2 type implementing Message interface
    - Define AddrV2Version constant (12)
    - Reject MsgAddr for negotiated protocol version >= 12
    - Add ErrTooFewAddrs error
    - Add test coverage

---
+++ wire/netaddressv2.go
@@ -0,0 +1,80 @@
+// Copyright (c) 2025 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package wire
+
+import (
+	"net"
+	"time"
+)
+
+// NetAddressType is used to indicate the type of a given network address.
+type NetAddressType uint8
+
+const (
+	UnknownAddressType NetAddressType = 0
+	IPv4Address        NetAddressType = 1
+	IPv6Address        NetAddressType = 2
+)
+
+// NetAddressV2 defines information about a peer on the network.
+//
+// The field order matches the wire protocol encoding order.
+type NetAddressV2 struct {
+	// Timestamp is the last time the address was seen.
+	Timestamp time.Time
+
+	// Services represents the service flags supported by this network address.
+	Services ServiceFlag
+
+	// Type represents the type of network that the network address belongs to.
+	Type NetAddressType
+
+	// IP address of the peer. It is defined as a byte array to support various
+	// address types that are not standard to the net package and therefore not
+	// entirely appropriate to store as a net.IP.
+	IP []byte
+
+	// Port is the port of the remote peer.
+	Port uint16
+}
+
+// NewNetAddressV2 creates a new network address using the provided
+// parameters without validation.
+func NewNetAddressV2(netAddressType NetAddressType, addrBytes []byte, port uint16, timestamp time.Time, services ServiceFlag) NetAddressV2 {
+	return NetAddressV2{
+		Timestamp: timestamp,
+		Services:  services,
+		Type:      netAddressType,
+		IP:        addrBytes,
+		Port:      port,
+	}
+}
+
+// NewNetAddressV2IPPort returns a new NetAddressV2 using the provided IP,
+// port, and supported services with a current timestamp. The address type is
+// automatically determined from the IP (IPv4 or IPv6).
+func NewNetAddressV2IPPort(ip net.IP, port uint16, services ServiceFlag) NetAddressV2 {
+	var addrType NetAddressType
+	var addrBytes []byte
+
+	if ip4 := ip.To4(); ip4 != nil {
+		addrType = IPv4Address
+		addrBytes = ip4
+	} else {
+		addrType = IPv6Address
+		addrBytes = ip.To16()
+	}
+
+	// Limit the timestamp to one second precision since the protocol
+	// doesn't support better.
+	timestamp := time.Unix(time.Now().Unix(), 0)
+	return NetAddressV2{
+		Timestamp: timestamp,
+		Services:  services,
+		Type:      addrType,
+		IP:        addrBytes,
+		Port:      port,
+	}
+}
