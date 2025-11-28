368157c6627002f9afa3e4826e10ff49bb862bc3
Dave Collins <davec@conformal.com>
2025-11-28 13:01:54 -0600

    [release-v2.1] release: Bump for 2.1.1.

--- internal/version/version.go
+++ internal/version/version.go
@@ -57,7 +57,7 @@ var (
 	// the package will panic at runtime.  Of particular note is the pre-release
 	// and build metadata portions MUST only contain characters from
 	// semanticAlphabet.
-	Version = "2.1.0+release.local"
+	Version = "2.1.1+release.local"
 
 	// NOTE: The following values are set via init by parsing the above Version
 	// string.
