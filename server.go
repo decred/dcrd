ac9eb0ab981117286d098b4bfe079594594fe8f3
Dave Collins <davec@conformal.com>
2025-12-19 14:12:33 -0600

    server: Simplify and correct ban score logging.
    Currently, the logic that accumulates the persistent and transient ban
    scores incorrectly logs debug messages alleging whitelisted peers are
    misbehaving due to perfectly normal accumulation of the decaying
    transient ban score.
    
    Also, it currently logs the overall ban score whenever it is over the
    warning threshold even when the score is not increasing.  There isn't
    anything incorrect about that behavior, and the comments even correctly
    call out that is what it is intended to do, but it also not very useful.
    
    This modifies the logic to address the aforementioned and takes the
    opportunity to simplify the code a bit.
    
    In particular, it now treats whitelisted peers the same as all other
    peers in terms of tracking the ban score and warning for misbehavior
    whenever the score exceeds the warning threshold.  However, when the ban
    score exceeds the ban threshold, whitelisted peers are exempted.
    
    The misbehavior warning messages that are shown once the ban score
    exceeds the warning threshold will now include the additional detail
    that the peer is whitelisted when that is the case.
    
    Finally, there is no longer any logging when the ban score is not
    increasing.

--- server.go
+++ server.go
@@ -1021,36 +1021,34 @@ func (sp *serverPeer) pushAddrMsg(addresses []*addrmgr.NetAddress) {
 }
 
 // addBanScore increases the persistent and decaying ban score fields by the
-// values passed as parameters. If the resulting score exceeds half of the ban
-// threshold, a warning is logged including the reason provided. Further, if
-// the score is above the ban threshold, the peer will be banned and
-// disconnected.
+// provided values unless banning is disabled.
+//
+// A warning is logged including the reason provided when the resulting score
+// exceeds half of the ban threshold and, unless the peer is whitelisted, it
+// will be banned and disconnected when the score exceeds the ban threshold.
 func (sp *serverPeer) addBanScore(persistent, transient uint32, reason string) bool {
 	// No warning is logged and no score is calculated if banning is disabled.
 	if cfg.DisableBanning {
 		return false
 	}
-	if sp.isWhitelisted {
-		srvrLog.Debugf("Misbehaving whitelisted peer %s: %s", sp, reason)
+
+	// Nothing to do when the score is not being increased.
+	if transient == 0 && persistent == 0 {
 		return false
 	}
 
-	warnThreshold := cfg.BanThreshold >> 1
-	if transient == 0 && persistent == 0 {
-		// The score is not being increased, but a warning message is still
-		// logged if the score is above the warn threshold.
-		score := sp.banScore.Int()
-		if score > warnThreshold {
-			srvrLog.Warnf("Misbehaving peer %s: %s -- ban score is %d, "+
-				"it was not increased this time", sp, reason, score)
-		}
-		return false
-	}
+	warnThreshold := cfg.BanThreshold / 2
 	score := sp.banScore.Increase(persistent, transient)
 	if score > warnThreshold {
-		srvrLog.Warnf("Misbehaving peer %s: %s -- ban score increased to %d",
-			sp, reason, score)
-		if score > cfg.BanThreshold {
+		peerStr := "peer"
+		if sp.isWhitelisted {
+			peerStr = "whitelisted peer"
+		}
+		srvrLog.Warnf("Misbehaving %s %s: %s -- ban score increased to %d",
+			peerStr, sp, reason, score)
+
+		// Ban the peer once the threshold is exceeded unless it is whitelisted.
+		if score > cfg.BanThreshold && !sp.isWhitelisted {
 			const reason = "ban score exceeds threshold"
 			sp.server.BanPeer(sp, reason)
 			return true
