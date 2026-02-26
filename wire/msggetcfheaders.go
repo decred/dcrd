1163dc2a351728543ea3166f56ffec7f3a18e121
Dave Collins <davec@conformal.com>
2026-02-25 22:09:55 -0600

    wire: Prepare v1.7.4.
    This retracts that 1.7.3 wire release, updates the wire module copyright
    year in the files modified since the previous wire/v1.7.2 release and
    serves as a base for wire/v1.7.4.

--- wire/msggetcfheaders.go
+++ wire/msggetcfheaders.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2017 The btcsuite developers
 // Copyright (c) 2017 The Lightning Network Developers
-// Copyright (c) 2018-2024 The Decred developers
+// Copyright (c) 2018-2026 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
