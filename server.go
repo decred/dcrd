664d136e45d29cf734c620e1c8b91f7797b57d1d
Dave Collins <davec@conformal.com>
2026-04-06 13:32:48 -0500

    [release-v2.1] server: Don't consider services before handshake.
    The sync manager considers the services when the initial peer is
    created, but the remote services are not known until the handshake
    completes.
    
    This adds a channel that is closed when a verack is received in order to
    wait for the handshake to complete prior to creating the sync manager
    peer to ensure the remote services are known and populated.
    
    It also moves the code that adds the peer to the server to happen after
    the handshake as well since it relies on the sync manager.
    
    This was discovered by changes that will be in the next commit to no
    longer improperly assume the remote services before they're known.

--- server.go
+++ server.go
@@ -676,6 +676,9 @@ type serverPeer struct {
 	isWhitelisted bool
 	quit          chan struct{}
 
+	handshakeDone          chan struct{}
+	closeHandshakeDoneOnce sync.Once
+
 	// syncMgrPeer houses the network sync manager peer instance that wraps the
 	// underlying peer similar to the way this server peer itself wraps it.
 	syncMgrPeer *netsync.Peer
@@ -731,6 +734,7 @@ func newServerPeer(s *server, isPersistent bool) *serverPeer {
 		persistent:     isPersistent,
 		knownAddresses: apbf.NewFilter(maxKnownAddrsPerPeer, knownAddrsFPRate),
 		quit:           make(chan struct{}),
+		handshakeDone:  make(chan struct{}),
 		getDataQueue:   make(chan []*wire.InvVect, maxConcurrentGetDataReqs),
 	}
 }
@@ -924,6 +928,9 @@ func (sp *serverPeer) Run() {
 		sp.serveGetData()
 		wg.Done()
 	}()
+
+	// Add valid peer to the server.
+	sp.server.AddPeer(sp)
 
 	// Wait for the peer to disconnect and notify the net sync manager and
 	// server accordingly.
@@ -1194,15 +1201,13 @@ func (sp *serverPeer) OnVersion(_ *peer.Peer, msg *wire.MsgVersion) {
 	// Add the remote peer time as a sample for creating an offset against
 	// the local clock to keep the network time in sync.
 	sp.server.timeSource.AddTimeSample(sp.Addr(), msg.Timestamp)
-
-	// Add valid peer to the server.
-	sp.server.AddPeer(sp)
 }
 
 // OnVerAck is invoked when a peer receives a verack wire message.  It creates
 // and sends a sendheaders message to request all block annoucements are made
 // via full headers instead of the inv message.
 func (sp *serverPeer) OnVerAck(_ *peer.Peer, msg *wire.MsgVerAck) {
+	sp.closeHandshakeDoneOnce.Do(func() { close(sp.handshakeDone) })
 	sp.QueueMessage(wire.NewMsgSendHeaders(), nil)
 }
 
@@ -2327,8 +2332,14 @@ func (s *server) inboundPeerConnected(conn net.Conn) {
 	sp := newServerPeer(s, false)
 	sp.isWhitelisted = isWhitelisted(conn.RemoteAddr())
 	sp.Peer = peer.NewInboundPeer(newPeerConfig(sp))
-	sp.syncMgrPeer = netsync.NewPeer(sp.Peer)
 	sp.AssociateConnection(conn)
+	select {
+	case <-sp.handshakeDone:
+	case <-time.After(30 * time.Second):
+		srvrLog.Debugf("Handshake timeout for inbound peer %s", conn.RemoteAddr())
+		return
+	}
+	sp.syncMgrPeer = netsync.NewPeer(sp.Peer)
 	go sp.Run()
 }
 
@@ -2346,10 +2357,17 @@ func (s *server) outboundPeerConnected(c *connmgr.ConnReq, conn net.Conn) {
 		return
 	}
 	sp.Peer = p
-	sp.syncMgrPeer = netsync.NewPeer(sp.Peer)
 	sp.connReq.Store(c)
 	sp.isWhitelisted = isWhitelisted(conn.RemoteAddr())
 	sp.AssociateConnection(conn)
+	select {
+	case <-sp.handshakeDone:
+	case <-time.After(30 * time.Second):
+		srvrLog.Debugf("Handshake timeout from outbound peer %s", c.Addr)
+		s.connManager.Disconnect(c.ID())
+		return
+	}
+	sp.syncMgrPeer = netsync.NewPeer(sp.Peer)
 	go sp.Run()
 }
 
