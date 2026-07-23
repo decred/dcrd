852180a6966884776500245471225efd90c620ef
jholdstock <jholdstock@decred.org>
2026-07-23 15:47:55 +0800

    wire: Add tests for MsgGetMiningState.
    MsgGetMiningState is the only non-mixing wire message which did not have
    an accompanying test file. The tests added in this commit copy from the
    existing tests for message types with no message body.

---
+++ wire/msggetminingstate_test.go
@@ -0,0 +1,94 @@
+// Copyright (c) 2026 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package wire
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
+// TestGetMiningState tests the MsgGetMiningState API.
+func TestGetMiningState(t *testing.T) {
+	pver := ProtocolVersion
+
+	// Ensure the command is expected value.
+	wantCmd := "getminings"
+	msg := NewMsgGetMiningState()
+	if cmd := msg.Command(); cmd != wantCmd {
+		t.Errorf("NewMsgGetMiningState: wrong command - got %v want %v",
+			cmd, wantCmd)
+	}
+
+	// Ensure max payload is expected value for latest protocol version.
+	wantPayload := uint32(0)
+	maxPayload := msg.MaxPayloadLength(pver)
+	if maxPayload != wantPayload {
+		t.Errorf("MaxPayloadLength: wrong max payload length for "+
+			"protocol version %d - got %v, want %v", pver,
+			maxPayload, wantPayload)
+	}
+
+	// Ensure max payload length is not more than MaxMessagePayload.
+	if maxPayload > MaxMessagePayload {
+		t.Fatalf("MaxPayloadLength: payload length (%v) for protocol "+
+			"version %d exceeds MaxMessagePayload (%v).", maxPayload, pver,
+			MaxMessagePayload)
+	}
+}
+
+// TestGetMiningStateWire tests the MsgGetMiningState wire encode and decode for
+// various protocol versions.
+func TestGetMiningStateWire(t *testing.T) {
+	msgGetMiningState := NewMsgGetMiningState()
+	msgGetMiningStateEncoded := []byte{}
+
+	tests := []struct {
+		in   *MsgGetMiningState // Message to encode
+		out  *MsgGetMiningState // Expected decoded message
+		buf  []byte             // Wire encoding
+		pver uint32             // Protocol version for wire encoding
+	}{
+		// Latest protocol version.
+		{
+			msgGetMiningState,
+			msgGetMiningState,
+			msgGetMiningStateEncoded,
+			ProtocolVersion,
+		},
+	}
+
+	t.Logf("Running %d tests", len(tests))
+	for i, test := range tests {
+		// Encode the message to wire format.
+		var buf bytes.Buffer
+		err := test.in.BtcEncode(&buf, test.pver)
+		if err != nil {
+			t.Errorf("BtcEncode #%d error %v", i, err)
+			continue
+		}
+		if !bytes.Equal(buf.Bytes(), test.buf) {
+			t.Errorf("BtcEncode #%d\n got: %s want: %s", i,
+				spew.Sdump(buf.Bytes()), spew.Sdump(test.buf))
+			continue
+		}
+
+		// Decode the message from wire format.
+		var msg MsgGetMiningState
+		rbuf := bytes.NewReader(test.buf)
+		err = msg.BtcDecode(rbuf, test.pver)
+		if err != nil {
+			t.Errorf("BtcDecode #%d error %v", i, err)
+			continue
+		}
+		if !reflect.DeepEqual(&msg, test.out) {
+			t.Errorf("BtcDecode #%d\n got: %s want: %s", i,
+				spew.Sdump(msg), spew.Sdump(test.out))
+			continue
+		}
+	}
+}
