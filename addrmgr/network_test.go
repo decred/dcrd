c7133173dfafda65eb6f9e849f6f3d5d7febf216
jholdstock <jholdstock@decred.org>
2026-07-31 13:25:33 +0800

    addrmgr: Fix incorrect fail msg on TestIPTypes.

--- addrmgr/network_test.go
+++ addrmgr/network_test.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2013-2014 The btcsuite developers
-// Copyright (c) 2015-2025 The Decred developers
+// Copyright (c) 2015-2026 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
@@ -125,7 +125,7 @@ func TestIPTypes(t *testing.T) {
 		}
 
 		if rv := isRFC6145(test.ip); rv != test.rfc6145 {
-			t.Errorf("isRFC1918 %s\n got: %v want: %v", test.ip, rv, test.rfc6145)
+			t.Errorf("isRFC6145 %s\n got: %v want: %v", test.ip, rv, test.rfc6145)
 		}
 
 		if rv := isLocal(test.ip); rv != test.local {
